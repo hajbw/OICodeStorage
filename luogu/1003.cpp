52af8f280f010483d6c8864f3e82723507cfcc30
hajbw <hajbw10@126.com>
2018-01-06 17:33:16 +0800

    update 1003.cpp(not accepted yet)

--- luogu/1003.cpp
+++ luogu/1003.cpp
@@ -11,26 +11,36 @@ struct carpet
 	int x,y,width,height;
 }
 
+bool isInside(int,int,carpet);
+
 int main()
 {
 	int amountOfCarpet,targetX,targetY,result;
 	carpet * carpets;
-    
+
 	cin>>amountOfCarpet;
-    
+
 	carpets = new carpet[amountOfCarpet];
 
 	for (int i = 0; i < amountOfCarpet; ++i)
 		cin>>carpets[i].x>>carpets[i].y>>carpets[i].width>>carpets[i].height;
 
 	for (result = amountOfCarpet; result > 0 ; result)
-	{
-		
+	{//result - 1 to access carpets[index]
+		if(isInside(targetX,targetY,carpets[result]))
 	}
+
+	if(result = 0)
+		result = -1;//ugly fix
 
 	cout<<result;
 
-	delete [] carpet;
+	delete [] carpets;
 
 	return 0;
+}
+
+bool isInside(int x,int y,carpet cover)
+{
+	return false;
 }
