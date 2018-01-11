cf45a3d6ea14b2f6342321496a81fd0ef2898090
hajbw <hajbw10@126.com>
2018-01-11 18:40:43 +0800

    new problem

---
+++ luogu/3987.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+/*
+	P3987 我永远喜欢珂朵莉~
+
+	输入格式：
+	第一行两个数表示n,m
+	第二行n个非负整数表示ai
+	之后m行每行一个操作
+	1 l r x : 把区间[l,r]中所有x的倍数/x
+	2 l r : 查询区间[l,r]的和
+
+	输出格式：
+	对于每次询问，输出一行一个数表示答案
+*/
+
+int query1(int left,int right ,int x);
+int query2(int left,int right);
+
+int main()
+{
+	int m,n
+	return 0;
+}
+
+int query1(int left,int right ,int x)
+{
+
+}
+
+int query2(int left,int right)
+{
+
+}
