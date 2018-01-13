d3fc0817d6120a012741db61887021f0bb346154
hajbw <hajbw10@126.com>
2018-01-13 20:57:47 +0800

    update files(still WA ;( )

--- codevs/1155.cpp
+++ codevs/1155.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
-using namespace std;
+/*
+	1155 金明的预算方案
+*/
 
 int main()
 {
+	int money_sum,items_hoped,result = 0;
+
+	std::cin>>money_sum>>items_hoped;
+
+	for (int i = 0; i < items_hoped; ++i)
+	{
+		
+	}
+
+	std::cout<<result;
 
 	return 0;
 }
