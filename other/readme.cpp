1dd2a07fec2c77a05113310a7821c04887b92c86
hajbw <hajbw10@126.com>
2018-01-15 01:55:57 -0600

    Update readme.cpp

--- other/readme.cpp
+++ other/readme.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <thread>
+#include <chrono> 
 
 /*
 leave for comer
@@ -10,6 +12,8 @@ using std::cin;
 
 int main()
 {
-
+    cout<<endl;
+    std::this_thread::sleep_for (std::chrono::seconds(1));
+    return 0;
 }
 
