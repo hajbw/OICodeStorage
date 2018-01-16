83f8871833f77eeff0f67f2a037935e37abbb122
hajbw <hajbw10@126.com>
2018-01-16 16:11:03 +0800

    former's left

---
+++ other/former.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+using namespace std;
+char k[7];
+int i;
+void isp ();
+int main () {
+	cout << "Hello World !" << endl;
+	Sleep (3840);
+	cout << "欢迎来到南宁三中。" << endl;
+	Sleep (1920);
+	cout << "这是一个各种设置基本齐全的学校。" << endl;
+	Sleep (1680);
+	cout << "各种设施啥的你自己去了解就行了。" << endl;
+	Sleep (1680);
+	cout << "另外，这里的快递行业发展得不错。" << endl;
+	Sleep (1680);
+	cout << "不过这些都不是这个文件要讲的重点。" << endl;
+	system ("pause");
+	system ("cls");
+
+	cout << "骚年，你知道你接下来至少一个学期内你要调戏的电脑的基本情况吗？" << endl;
+	Sleep (2160);
+	cout << "想看看它的主要硬件配置吗？" << endl;
+	Sleep (1680);
+	cout << "(请输入Y／N)" << endl;
+	cin >> k[i];
+	system ("cls");
+	if (k[i] == 'Y') cout<<"test01";//CPUZ.exe;
+	i++;
+	system ("pause");
+	system ("cls");
+
+	cout << "接下来，说说这台电脑上的病毒。" << endl;
+	Sleep (1680);
+	cout << "这台电脑上有以下两种病毒，" << endl;
+	Sleep (1280);
+	cout << "一种会隐藏你U盘根目录下的所有文件或文件夹，并且建立同名快捷方式。" << endl;
+	Sleep (1280);
+	cout << "另一种会把你U盘根目录下的所有exe应用程序文件替换为大小1.3MB的疑似病毒程序" << endl;
+	system ("pause");
+	cout << "别鸟那个B度杀毒，它根本没用" << endl;
+	Sleep (1680);
+	system ("cls");
+	cout << "另外，我还藏了一个wanna cry在电脑里面（当然是存在压缩包里面啦），如果你想要的话，这里有密码。" << endl;
+	Sleep (2560);
+	cout << "想知道密码吗？这个密码同时也是学校机房的极域等很多地方的密码哦！" << endl;
+	Sleep (1280);
+	cout << "(请输入Y／N)" << endl;
+	cin >> k[i];
+	system ("cls");
+	if (k[i] == 'Y') cout<<"test01";//isp();
+	i++;
+	system ("pause");
+	system ("cls");
+
+	cout << "网络方面，很遗憾，这台电脑上不了外网，估计是学校搞了劫持" << endl;
+	system ("pause");
+	system ("cls");
+
+	cout << "6.5 mm 音频接口有点小问题，不能很好地使用。" << endl;
+	Sleep (1024);
+	cout << "3.5 mm 接口要把输入切换到笔记本电脑不说，音量貌似出奇的小。" << endl;
+	Sleep (1024);
+	cout << "所以要放歌还是老老实实用电脑吧。" << endl;
+	system ("pause");
+	system ("cls");
+
+
+	cout << "这台电脑上还有很多有趣的东西，需要你自己去发现哦。" << endl;
+	system ("pause");
+	system ("cls");
+
+	cout << "另外，年轻人，你想听取一些人生经验吗？" << endl;
+	Sleep (1024);
+	cout << "(请输入Y／N)" << endl;
+	cin >> k[i];
+	if (k[i] == 'Y') cout<<"test01";//jzm.exe;
+	i++;
+
+	return 0;
+}
+
+void isp () {
+	cout << "密码嘛，你猜。" << endl;
+}
+
