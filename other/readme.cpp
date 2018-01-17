5905f404997a2fe4a3a85c1852da3626579892d1
hajbw <hajbw10@126.com>
2018-01-17 21:25:32 +0800

    update the stuff for comers

--- other/readme.cpp
+++ other/readme.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <thread>
-#include <chrono> 
+#include <chrono>
 
 /*
 leave for comer
@@ -12,8 +13,32 @@ using std::cin;
 
 int main()
 {
-    cout<<endl;
-    std::this_thread::sleep_for (std::chrono::seconds(1));
-    return 0;
+	ifstream input("readme");
+	char * line,ch;
+	int index;
+
+	//testing code
+	cout<<endl;
+	std::this_thread::sleep_for (std::chrono::seconds(1));
+
+	//real stuff
+/*
+	while(input.readline())
+	{
+
+	while(ch = line[index]){
+		cout.put(ch);
+		std::this_thread::sleep_for (std::chrono::miliseconds(100));
+		index++;
+	}
+
+	std::this_thread::sleep_for (std::chrono::seconds(1));
+
+
+	}
+
+*/
+
+	return 0;
 }
 
