60ac58d8bc3bc994247889005027199175dd9d17
hajbw@neoterm@MHA-AL00 <hajbw10@126.com>
2018-01-21 16:14:00 +0800

    rdme-4-comers update v0.4.3

--- other/readme.cpp
+++ other/readme.cpp
@@ -2,13 +2,14 @@
 #include <fstream>
 #include <thread>
 #include <chrono>
+#include <windows.h>
 
 /*
 leave for comer
 
 author:hajbw
 email:hajbw10@126.com
-version:0.4.2
+version:0.4.3
 last changed date:Jan 21,2018
 release date:
 
@@ -22,25 +23,25 @@ int main()
 {
 	ifstream readme_part1("README");
 	ifstream readme_signature("README_SIGNATURE");
-	char * line,ch;
-	int index;
+	char ch;
 
 	//testing code
 	cout<<endl;
 	std::this_thread::sleep_for (std::chrono::seconds(1));
 
 	//real stuff
-	//read per line of readme then show
+	//read char by char
 	//notice: 
 	//$ in readme means to pause
 	//# to cls
-	//- for comment(ignore the rest of this line)
+	//' for comment(ignore the rest of this line)
+	//- to stop for .5 second
 
 	while(ch = readme_part1.get())
 	{
 		switch(ch)
 		{
-		case'$'://change into switch!
+			case'$':
 			system("pause");
 			break;
 		case'#':
@@ -49,9 +50,12 @@ int main()
 		case'-':
 			std::this_thread::sleep_for (std::chrono::miliseconds(500));
 			break;
+		case'\'':
+			while(ch != '\n')
+				ch.get();
+			break;
 		default:
 			cout.put(ch);
-			index++;
 		}
 		std::this_thread::sleep_for (std::chrono::miliseconds(100));
 
