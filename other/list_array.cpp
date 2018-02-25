932296c2759be6d9f0135c9f55acbfef8edabd53
hajbw@neoterm@MHA-AL00 <hajbw10@126.com>
2018-02-26 07:35:00 +0800

    add a array-based-list cpp (for learning)

---
+++ other/list_array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namepace std;
+
+//add a status code (or exception throw)to return!
+void sort(int* array,int length);
+void insert(int*,int,int,int,int);
+
+int main()
+{
+	int[] arr;
+	int arr_current_size = 0,arr_data_length = 0;
+
+	return 0;
+}
+
+
+void sort(int* array,int length)
+{
+	return 0;
+}
+
+
+void insert(int* array,int length,int size,int element,int index)
+{
+
+
+	return 0;
+}
+
+
