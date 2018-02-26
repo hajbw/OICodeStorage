7e755a0cf67a37be8014c9005f079e926663df06
hajbw@neoterm@MHA-AL00 <hajbw10@126.com>
2018-02-26 18:38:21 +0800

    update queue.cpp

--- other/queue.cpp
+++ other/queue.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+struct queue_node
+{
+	int data;
+	queue_node *next;
+}
+
+class queue
+{
+	private:
+		queue_node *head,*tail;
+	public:
+		queue_node* get_head()
+		{
+			return head;
+		}
+		queue_node* pop_head()
+		{
+		
+		}
+}
+
+
 int main()
 {
 
