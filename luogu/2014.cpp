d823ee082e44b903b451b62fd369a713db243652
hajbw@USB <hajbw10@126.com>
2018-03-10 17:03:41 +0800

    update luogu/2014.cpp(not run yet)

--- luogu/2014.cpp
+++ luogu/2014.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 /*
-
 	P2014 选课
-
 */
 
-struct node
-{
-	node * child, * next;
-	int data;
-};
+int value[310],f[310][310],son[310] {0},next[310],m,n;
 
 int main()
 {
+	//I'm not sure if f[][]{0} works
+	memset(f,0,sizeof(f));
+	//input and build
+	cin>>n>>m;
+
+	int father;
+	for (int i = 1; i <= n; ++i)
+	{
+		cin>>father>>value[i];
+		next[i] = son[father];
+		son[father] = i;
+	}
 
 	return 0;
 }
+
