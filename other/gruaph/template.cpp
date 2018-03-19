096610540feb78d8161920dc7513960b7119f75d
hajbw@USB <hajbw10@126.com>
2018-03-19 18:57:07 +0800

    update graph template

--- other/gruaph/template.cpp
+++ other/gruaph/template.cpp
@@ -9,46 +9,44 @@ struct Edge
 	int from,to,weight;
 };
 
-bool compare_by_weight(Edge i,Edge j){return i.weight<j.weight;}
+//bool compare_by_weight(Edge i,Edge j){return i.weight<j.weight;}
 int find_root(int);
 
-Edge * edges; 
-int  *parent,point_count,edge_count;
+Edge edge[200000]; 
+int  parent[5000],point_count,edge_count;
 
 
 int main()
 {
-	int result = 0,chosen_edge_count = 0,temp_edge_index = 0,a,b;
-	cin>>point_count>>edge_count;
+	int result = 0,chosen_edge_count = 0,edge_index = 0,a,b;
 
-	parent = new int[point_count];
-	edges = new Edge[edge_count];
+	cin>>point_count>>edge_count;
 
 	for (int i = 0; i < point_count; ++i)parent[i] = i;
 
 	for (int i = 0; i < edge_count; ++i)
 	{
-		cin>>edges[i].from>>edges[i].to>>edges[i].weight;
+		cin>>edge[i].from>>edge[i].to>>edge[i].weight;
 	}
 
-	sort(edges,edges + edge_count,compare_by_weight);
+	sort(edge,edge + edge_count,[](Edge i,Edge j) -> bool{return i.weight<j.weight;});
 
 	//find it now!
-	while(chosen_edge_count < edge_count - 1)
+	while(chosen_edge_count < point_count - 1)
 	{
-		a = find_root(edges[temp_edge_index].from);
-		b = find_root(edges[temp_edge_index].to);
+		a = find_root(edge[edge_index].from);
+		b = find_root(edge[edge_index].to);
 
 		if(a != b)
 		{
 			parent[a] = b;
-			result += edges[temp_edge_index].weight;
+			result += edge[edge_index].weight;
 			++chosen_edge_count;
 		}
 
-		++temp_edge_index;
+		++edge_index;
 
-		/*if(temp_edge_index == edge_count)
+		/*if(edge_index == edge_count)
 		{
 			cout<<"orz";
 		}*/
@@ -56,9 +54,6 @@ int main()
 	}
 
 	cout<<result;
-
-	delete [] parent;
-	delete [] edges;
 
 	return 0;
 
