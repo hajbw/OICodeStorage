134fe4d85953257ddfb76c5bc1020c2f9637343e
hajbw@USB <hajbw10@126.com>
2018-03-21 17:46:57 +0800

    update 1909.cpp

--- luogu/1909.cpp
+++ luogu/1909.cpp
@@ -6,28 +6,22 @@ using namespace std;
 	P1909 买铅笔
 */
 
-
-struct good_set
-{
-	int amount_of_pencil,price;
-};
-
 int main()
 {
-	int i,j,pencils_needed,result = 0,pencils[10002];
+	int amount[3],price[3],i,j,pencils_needed,result = 0,pencils[10002];
 	good_set pencilset[3];
 
 	cin>>pencils_needed;
 	for (i = 0; i < 3; ++i)
-		cin>>pencilset[i].amount_of_pencil>>pencilset[i].price;
+		cin>>price[i]>>pencilset[i].price;
 
 	for (i = 0; i < 3; ++i)
 	{
-		for (j = pencils_needed; j >= pencilset[i].amount_of_pencil ; --j)
+		for (j = pencils_needed; j >= price[i] ; --j)
 		{
 			pencils[i] = (
-				pencils[i] < pencils[i - pencilset[i].amount_of_pencil] + pencilset[i].price ?
-				pencils[i - pencilset[i].amount_of_pencil] + pencilset[i].price :
+				pencils[i] < pencils[i - price[i]] + price[i] ?
+				pencils[i - price[i]] + price[i] :
 				pencils[i]
 				);
 		}
