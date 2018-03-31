b054adefd29809603e638ef106d7803299e9eba6
hajbw@school <hajbw10@126.com>
2018-03-31 16:54:20 +0800

    small update of 1017.cpp

--- luogu/1017.cpp
+++ luogu/1017.cpp
@@ -3,14 +3,18 @@
 
 using namespace std;
 
-inline char map2char(int i){}
-
 int main()
 {
 	int input,base,temp;
+	char map2char[20]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J'};
 	vector<char> result;
 
 	cin>>input>>base;
+
+	cout<<input<<"=";
+	for(vector<char>::iterator i = result.begin(); i != result.end(); ++i)
+		cout<<*i;
+	cout<<"(base"<<base<<")";
 
 	return 0;
 }
