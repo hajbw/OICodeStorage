f1092c6e704f9fe17ce1190708bb0300c98f6b79
hajbw@school <hajbw10@126.com>
2018-03-31 17:10:37 +0800

    update dj.cpp

--- other/gruaph/dijkstra.cpp
+++ other/gruaph/dijkstra.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
-const int MAX_V = 10000,MAX_E;
+const int MAX_V = 100,MAX_E = 500;
 
 int diatance[MAX_V],parent[MAX_V];
 bool isFinished[MAX_V];
+int adajacency_list[MAX_V][MAX_V];
 
 int main()
 {
