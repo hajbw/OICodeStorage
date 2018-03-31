4acb8b71304446e7f5aac3347928c281dd0fd3db
hajbw@school <hajbw10@126.com>
2018-03-31 16:17:31 +0800

    add 1017.cpp(to-do-list++ (( ;( )

---
+++ luogu/1017.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+inline char map2char(int i){}
+
+int main()
+{
+	int input,base,temp;
+	vector<char> result;
+
+	cin>>input>>base;
+
+	return 0;
+}
