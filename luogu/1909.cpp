acfafe1a2410518c6d14e7ddc9fa7ae2a1b1de75
hajbw <hajbw10@126.com>
2018-04-01 17:37:56 +0800

    Update 1909.cpp

--- luogu/1909.cpp
+++ luogu/1909.cpp
@@ -8,19 +8,23 @@ using namespace std;
 
 int main()
 {
-	int amount[3],price[3],i,j,pencils_needed,result = 0,pencils[10002];
-	good_set pencilset[3];
+	int amount[3],price[3],order[3],i,ii,j,pencils_needed,result = 0,pencils[10002];
 
 	cin>>pencils_needed;
 	for (i = 0; i < 3; ++i)
-		cin>>price[i]>>pencilset[i].price;
-
-	for (i = 0; i < 3; ++i)
 	{
+		cin>>price[i]>>pencilset[i].price;
+	}
+	
+	//todo: complete order[];
+
+	for (ii = 0; ii < 3; ++ii)
+	{
+		i = order[ii];
 		for (j = pencils_needed; j >= price[i] ; --j)
 		{
 			pencils[i] = (
-				pencils[i] < pencils[i - price[i]] + price[i] ?
+				pencils[i] > pencils[i - price[i]] + price[i] ?
 				pencils[i - price[i]] + price[i] :
 				pencils[i]
 				);
