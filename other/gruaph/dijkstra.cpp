777ebabc643800643b65ed0a55656f3259b988dc
hajbw <hajbw10@126.com>
2018-04-01 17:55:12 +0800

    Update dijkstra.cpp

--- other/gruaph/dijkstra.cpp
+++ other/gruaph/dijkstra.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <climits>
 
+/*
+	P1529 回家 Bessie Come Home
+*/
+
 using namespace std;
 
-const int MAX_V = 100,MAX_E = 500;
+const int MAX_V = 53,MAX_E = 10000;
 
 int diatance[MAX_V],parent[MAX_V];
 bool isFinished[MAX_V];
@@ -11,6 +15,13 @@ int adajacency_list[MAX_V][MAX_V];
 
 int main()
 {
-
+	int count,temp;
+	char a,b;
+	
+	cin>>count;
+	
+	for(int i = 0;i<count;++i)
+	
+	
 	return 0;
 }
