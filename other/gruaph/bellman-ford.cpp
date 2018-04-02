7415a8d6915ade85d47305f0b6338bdbb199c531
hajbw@school <hajbw10@126.com>
2018-04-02 18:00:21 +0800

    add bellman-forg template

---
+++ other/gruaph/bellman-ford.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+
+/*
+	bellman-ford template
+	now for luogu p1073
+*/
+
+const int MAX_V = 100000,MAX_E = 500000;
+
+using namespace std;
+
+//adajency list
+int degree[MAX_V];
+
+int main()
+{
+
+	return 0;
+}
