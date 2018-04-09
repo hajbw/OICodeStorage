ba6c134355678b7184fb2a95244b808cf55f07a3
hajbw@school <hajbw10@126.com>
2018-04-09 17:26:58 +0800

    add luogu P1529

---
+++ luogu/1529.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+
+/*
+	P1529 回家 Bessie Come Home
+*/
+
+const int MAX_V = 100000,MAX_E = 500000;
+
+using namespace std;
+
+//adajency list
+int degree[MAX_V];
+
+int main()
+{
+
+	return 0;
+}
