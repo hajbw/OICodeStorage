a573bd2d3fb7255dca1ccc85fbeb814e667593d5
hajbw@school <hajbw10@126.com>
2018-04-14 16:21:53 +0800

    add luogu P1073

---
+++ luogu/1073.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstring>
+
+/*
+	P1073 最优贸易
+*/
+
+using namespace std;
+
+const int MAX_V = 100000 ,MAX_E = 500000;
+
+int Vcount,Ecount,
+	x,y,z,e = 0,result = 0,
+	prize[MAX_V],gruaph[MAX_E * 2][2],buy[MAX_V],sell[MAX_V];
+
+
+int main()
+{
+	
+	//input
+	cin>>Vcount>>Ecount;
+	for (int i = 0; i < Vcount; ++i)
+		cin>>prize[i];
+	for (int i = 0; i < Ecount; ++i)
+	{
+		cin>>x>>y>>z;
+		++e;
+		gruaph[e][0] = x;
+		gruaph[e][1] = y;
+		if(z == 2)
+		{
+			++e;
+			gruaph[e][0] = y;
+			gruaph[e][1] = x;
+		}
+	}
+
+	for (int i = 0; i < Vcount; ++i)
+		sell[i] = buy[i] = prize[i];
+
+	//bellman-ford 4 buying
+	for (int i = 0; i < Vcount; ++i)
+	{
+		for (int j = 0; j < e; ++j)
+		{
+			if(buy[gruaph[j][1]] > buy[gruaph[j][0]])
+				buy[gruaph[j][1]] = buy[gruaph[j][0]];
+		}
+	}
+
+	for (int i = 0; i < Vcount; ++i)
+	{
+		for (int j = 0; j < e; ++j)
+			if(sell[gruaph[j][1]] < sell[gruaph[j][0]])
+				sell[gruaph[j][1]] = sell[gruaph[j][0]];
+	}
+
+	//output
+	for (int i = 0; i < Ecount; ++i)
+	{
+		e = sell[i] - buy[i];
+		result = e < result ? e : result;
+	}
+	cout<<result;
+
+	return 0;
+}
