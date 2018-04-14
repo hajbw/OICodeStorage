1c36585179619f91e9471aeab99718a18b31b16e
hajbw@school <hajbw10@126.com>
2018-04-14 16:47:58 +0800

    update luogu P1073

--- luogu/1073.cpp
+++ luogu/1073.cpp
@@ -11,7 +11,8 @@ const int MAX_V = 100000 ,MAX_E = 500000;
 
 int Vcount,Ecount,
 	x,y,z,e = 0,result = 0,
-	prize[MAX_V],gruaph[MAX_E * 2][2],buy[MAX_V],sell[MAX_V];
+	gruaph[MAX_E * 2][2],
+	prize[MAX_V],buy[MAX_V],sell[MAX_V];
 
 
 int main()
@@ -38,28 +39,29 @@ int main()
 	for (int i = 0; i < Vcount; ++i)
 		sell[i] = buy[i] = prize[i];
 
-	//bellman-ford 4 buying
 	for (int i = 0; i < Vcount; ++i)
 	{
 		for (int j = 0; j < e; ++j)
 		{
 			if(buy[gruaph[j][1]] > buy[gruaph[j][0]])
 				buy[gruaph[j][1]] = buy[gruaph[j][0]];
+			if(sell[gruaph[j][1]] < sell[gruaph[j][0]])
+				sell[gruaph[j][1]] = sell[gruaph[j][0]];
 		}
 	}
-
+/*
 	for (int i = 0; i < Vcount; ++i)
 	{
 		for (int j = 0; j < e; ++j)
 			if(sell[gruaph[j][1]] < sell[gruaph[j][0]])
 				sell[gruaph[j][1]] = sell[gruaph[j][0]];
 	}
-
+*/
 	//output
-	for (int i = 0; i < Ecount; ++i)
+	for (int i = 0; i < e; ++i)
 	{
 		e = sell[i] - buy[i];
-		result = e < result ? e : result;
+		result = e > result ? e : result;
 	}
 	cout<<result;
 
