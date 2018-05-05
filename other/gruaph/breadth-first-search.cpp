2ef71ac74e40fab471321ff0a52d7634a4f96087
hajbw@school <hajbw10@126.com>
2018-05-05 15:38:53 +0800

    add a BFS template

---
+++ other/gruaph/breadth-first-search.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+const MAXV = 100;MAXE = 150;
+
+std::vector<int> adjacency_list[MAXV];
+
+int main()
+{
+	int n,a,b,c;
+	
+	cin>>n;
+	for (int i = 0; i < n; ++i)
+	{
+		//format: from to lenth
+		cin>>a>>b>>c;
+		adjacency_list[a]
+	}
+
+	return 0;
+}
