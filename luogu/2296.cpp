2163abaf4f6dde86822042fb4c569c878307639c
hajbw@school <hajbw10@126.com>
2018-05-05 17:01:51 +0800

    update luogu P2296

--- luogu/2296.cpp
+++ luogu/2296.cpp
@@ -2,9 +2,25 @@
 #include <vector>
 
 /*
-	P2296 寻找道路
+	P2296 寻找道路	
 */
 
 using std::cin;
 using std::cout;
 using std::endl;
+
+const int MAXV = 10000,MAXE = 200000;
+
+//unweighted graph
+std::vector<int> adj_list[MAXV];
+int m,n,a,b;
+
+int main()
+{
+	cin>>n>>m;
+	for (int i = 0; i < m; ++i)
+	{
+		cin>>a>>b;
+		adj_list[a].push_back(b);
+	}
+}
