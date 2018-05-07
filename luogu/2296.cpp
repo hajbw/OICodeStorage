c232a164d0422101f2dee13d2f2fa6f48a120896
hajbw@neoterm@MHA-AL00 <hajbw10@126.com>
2018-05-07 19:50:36 +0800

    update luogu P2296

--- luogu/2296.cpp
+++ luogu/2296.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <cstring>
 
 /*
 	P2296 寻找道路	
@@ -12,19 +14,55 @@ using std::endl;
 const int MAXV = 10000,MAXE = 200000;
 
 //unweighted graph!
-std::vector<int> adj_list[MAXV];
+//std::vector<int> adj_list[MAXV];
 std::vector<int> rev_adj_list[MAXV];
-int m,n,a,b,result = 19260817;
+int available[MAXV] = {-1},dist[MAXV];
 
+void DFS(int i)
+{
+	if(available[i] = -1)
+		return;
+	available[i] = 1;
+	for(std::vector<int>::iterator it = rev_adj_list[i].begin();it != rev_adj_list[i].end();++it)
+	{
+		DFS(*it);
+	}
+}
 
+void DFS_exclude(int i)
+{
+
+}
 
 int main()
 {
+	int m,n,a,b,i,result = 19260817;
+	
+	memset(dist,19260817,sizeof(dist));
+
+	//Vertice,Edge
 	cin>>n>>m;
-	for (int i = 0; i < m; ++i)
+	for (i = 0; i < m; ++i)
 	{
 		cin>>a>>b;
-		adj_list[a].push_back(b);
+		//adj_list[a].push_back(b);
+		rev_adj_list[b].push_back(a);
 	}
-	cin>>a>>b;
+	cin>>a>>b;//s,t
+
+	//DFS rev-graph
+	DFS(b);
+	
+	for(i = 0;i < n;++i)
+		if(!available[i]);
+			
+	
+	//SPFA
+	std::queue<int> que;
+	que.push(a);
+	
+	
+	cout<<result;
+	
+	return 0;
 }
