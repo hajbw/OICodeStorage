c428b09ea87ef01a2b5d611062bb016608dea2f2
hajbw@school <hajbw10@126.com>
2018-05-14 18:51:40 +0800

    add nnszoj #2

---
+++ nnszoj/#2[STL].cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::find;
+
+const int MAXN = 100000;
+
+int arr[MAXN];
+
+int main()
+{
+	int n;
+	cin>>n;
+	
+}
---
+++ nnszoj/#2[custom].cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+const int MAXN = 100000;
+
+int arr[MAXN];
+
+int main()
+{
+	int i = 0,n,k,
+	low = 0,high,mid;//indexes
+	cin>>n;
+	for(;i < n;++i)
+		cin>>arr[i];
+	cin>>k;
+	
+	high = n;
+	mid = (low + high + 1)/2;
+	
+	while(arr[mid] != k)
+	{
+		if(k > arr[mid])
+			low = mid;
+		else
+			high = mid;
+		if(low + 1 == high)
+		{
+			cout<<-1;
+			return 0;
+		}
+		mid = (low + high + 1)/2;
+	}
+	
+	cout<<mid + 1;
+	
+	return 0;
+}
