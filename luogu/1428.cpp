dcac9b33d07fdab27a16b800a03b7a51a3531ee6
hajbw@neoterm@MHA-AL00 <hajbw10@126.com>
2018-05-14 19:22:01 +0800

    add luogu P1428

---
+++ luogu/1428.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+const int MAXN = 100;
+
+int fish_kawaii[MAXN],kawaii_fish[MAXN];
+
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i = 0;i < n;++i)
+		cin>>fish_kawaii[i];
+
+	return 0;
+
+}
