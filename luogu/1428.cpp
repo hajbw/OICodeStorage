4b4ad548523b9c4439c4c6ea8ae3ab475ecd8ed9
hajbw@ITGroup <hajbw10@126.com>
2018-05-15 11:03:12 +0800

    update luogu P1428 (AC)

--- luogu/1428.cpp
+++ luogu/1428.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 
+/*
+	P1428 小鱼比可爱
+	已AC
+*/
+
 using std::cin;
 using std::cout;
 
 const int MAXN = 100;
 
-int fish_kawaii[MAXN],kawaii_fish[MAXN];
+int fish_kawaiility[MAXN],kawaiility_fish[MAXN];
 
 int main()
 {
-	int n;
+	int n,kawaiility,fishes;
 	cin>>n;
 	for(int i = 0;i < n;++i)
-		cin>>fish_kawaii[i];
+	{
+		cin>>kawaiility;
+		fish_kawaiility[i] = kawaiility;
+		++kawaiility_fish[kawaiility];
+
+		fishes = 0;
+		for (int i = 0; i < kawaiility; ++i)
+			fishes += kawaiility_fish[i];
+		cout<<fishes<<" ";
+	}
+
 
 	return 0;
 
