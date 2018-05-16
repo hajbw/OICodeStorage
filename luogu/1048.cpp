bf4e1b5475fd9d1c7c087cfa3e7aceb6bd844c2f
hajbw@school <hajbw10@126.com>
2018-05-16 18:25:41 +0800

    update luogu P1048(AC)

--- luogu/1048.cpp
+++ luogu/1048.cpp
@@ -1,41 +1,40 @@
 #include <iostream>
 #include <algorithm>
-#include <cstring>
 
 /*
 	P1048 采药
+	AC
 */
 
 using std::cin;
 using std::cout;
 using std::sort;
+using std::max;
 
-const int MAXM = 100,MAXT = 1000;
+const int MAXM = 100,MAXT = 1001;
 
 struct Med
 {
 	int value,cost;
 }meds[MAXM];
 
-int f[MAXT];
+int f[MAXT];//max_value(cost)
 
 int main()
 {
-	int T,M;
+	int T,M;//time meds
 
 	cin>>T>>M;
 	for (int i = 0; i < M; ++i)
 		cin>>meds[i].cost>>meds[i].value;
 
-	sort(meds,meds + M,[](Med a,Med b) -> bool{return a.cost<b.cost;});
+	sort(meds,meds + M,[](Med a,Med b) -> bool{return a.cost < b.cost;});
 
-	for (int i = 0; i < M; ++i)
-	{
-		for (int j = meds[i].cost; j < T; ++j)
-		{
-			f[j] = 
-		}
-	}
+	for (int i = 0; i < M; ++i)//every med
+		for (int j = T; j >= meds[i].cost; --j)//time cost -> T
+			f[j] = max(f[j],f[j - meds[i].cost] + meds[i].value);
+
+	cout<<f[T];
 
 	return 0;
 }
