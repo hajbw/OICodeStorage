78790d164f505ff4f52f51c4e1038c7a6329c8ef
hajbw@school <hajbw10@126.com>
2018-05-23 19:13:50 +0800

    add 2nd solution of #16

---
+++ nnszoj/#16[2].cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/*
+	#16. 第一类斯特林数
+*/
+
+using std::cin;
+using std::cout;
+
+typedef unsigned long num;
+
+const int mo = 10000007;//ha
+
+num stir[500][500];
+
+int main()
+{
+	num m,n;
+
+	cin>>m>>n;
+
+
+}
