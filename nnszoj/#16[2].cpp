01034f2b36a9c764d7b93d84de58008bb68e4d5a
hajbw@school <hajbw10@126.com>
2018-05-26 16:19:46 +0800

    update sgqoj#16 (AC) (HHOJ)

--- nnszoj/#16[2].cpp
+++ nnszoj/#16[2].cpp
@@ -2,12 +2,13 @@
 
 /*
 	#16. 第一类斯特林数
+	AC
 */
 
 using std::cin;
 using std::cout;
 
-typedef unsigned long num;
+typedef unsigned long long num;
 
 const int mo = 10000007;//ha
 
@@ -17,7 +18,15 @@ int main()
 {
 	num m,n;
 
-	cin>>m>>n;
+	cin>>n>>m;
 
+	for (int i = 0; i <= m; ++i)
+		stir[i][i] = 1;
+
+	for (int i = 2; i <= n; ++i)
+		for (int j = 1; j <= m && j < i + 1; ++j)
+			stir[i][j] = ( stir[i - 1][j - 1] + stir[i - 1][j] * (i - 1) ) % mo;
+
+	cout<<stir[n][m];
 
 }
