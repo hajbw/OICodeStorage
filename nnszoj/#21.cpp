a64e8960c1331ca18b09f6439322509e3ed545fd
hajbw@school <hajbw10@126.com>
2018-05-26 17:22:27 +0800

    update nnszoj#21

--- nnszoj/#21.cpp
+++ nnszoj/#21.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
+/*
+	#21. 哈夫曼编码
+*/
+
 using std::cin;
 using std::cout;
 
+struct hf_node
+{
+	int weight;
+};
+
+int ascii_table[256];
+
 int main()
 {
-	
+	char ch;
+	int n = 0;//types of chars
+
+	while(cin.get(ch))
+		++ascii_table[(int)ch];
+
+	for (int i = 0; i < 256; ++i)
+		if(ascii_table[i])
+		{
+			++n;
+		}
+
+	return 0;
 }
