e9ee18b518b7daabd82b7b024d9b5383adb0ad58
hajbw@school <hajbw10@126.com>
2018-06-09 17:40:49 +0800

    update luogu P3373

--- luogu/3373.cpp
+++ luogu/3373.cpp
@@ -62,17 +62,17 @@ int main()
 		if(op_type == OP_MUL)
 		{
 			cin>>x>>y>>k;
-			multiply(x,y,k);
+			multiply(1,x,y,k);
 		}
 		else if(op_type == OP_ADD)
 		{
 			cin>>x>>y>>k;
-			add(x,y,k);
+			add(1,x,y,k);
 		}
 		else
 		{
 			cin>>x>>y;
-			cout<<query(x,y);
+			cout<<query(1,x,y);
 		}
 	}
 
@@ -96,13 +96,28 @@ void bulid_tree(int index,int left,int right)
 
 void multiply(int index,int left,int right,long long num)
 {
-
+	if(tree[index].left == left && tree[index].right == right)
+		
 }
 
 void add(int index,int left,int right,long long num)
 {
 	if(tree[index].left == left && tree[index].right == right)
 		tree[index].delay += num;
+	
+	int mid = tree[index].left + tree[index].right >> 1;
+
+	if(right <= mid)
+	{
+		add(index << 1,left,right,num);
+		return;
+	}
+	if(left > mid)
+	{
+		add(index << 1 | 1,left,right,num);
+	}
+	add(index << 1,left,mid,num);
+	add(index << 1 | 1,mid + 1,right,num);
 }
 
 int query(int index,int left,int right)
@@ -120,5 +135,9 @@ int query(int index,int left,int right)
 		return (query(index << 1 | 1,left,right) +
 			tree[index].delay * (right - left + 1)) % P;
 
-
+	return (
+		query(index << 1,left,mid) +
+		query(index << 1 | 1,mid + 1,right) +
+		tree[index].delay * (right - left + 1)
+		) % P;
 }
