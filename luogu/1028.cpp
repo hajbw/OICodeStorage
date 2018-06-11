da6a859ca42655bda10494d0940fcb6e6002e02d
hajbw@ITGroup <hajbw10@126.com>
2018-06-11 17:49:48 +0800

    update luogu P1028 (AC)

--- luogu/1028.cpp
+++ luogu/1028.cpp
@@ -2,16 +2,26 @@
 
 /*
 	P1028 数的计算
+	AC
 */
 
 using std::cin;
 using std::cout;
+
+const int MAXN = 1001;
+
+int ans[MAXN] = {1,1,2,2};
 
 int main()
 {
 	int n;
 	
 	cin>>n;
+
+	for(int i = 4;i <= n; i += 2)
+		ans[i] = ans[i | 1] = ans[i - 1] + ans[i >> 1];
+
+	cout<<ans[n];
 
 	return 0;
 }
