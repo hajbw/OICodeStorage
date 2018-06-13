51ac00018fc2f7af334f642d1d58592ac5df4cc7
hajbw@ITG2 <hajbw10@126.com>
2018-06-13 18:56:31 +0800

    update luogu P3952

--- luogu/3952.cpp
+++ luogu/3952.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 #include <stack>
+#include <set>
 
 /*
 	* P3952 时间复杂度
@@ -9,24 +11,15 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::stack;
+using std::set;
 
-struct loop
-{
-	char var_name;
-	int complexity;
-
-	loop(char var_name,int complexity)
-	{
-		this.var_name = var_name;
-		this.complexity = complexity;
-	}
-};
 
 int main()
 {
-	int count,lines_per_program,complexity;
-	char ch,*line;
-	stack<loop> loops;
+	int count,lines_per_program,est_complexity,complexity;
+	char ch,line[20];
+	stack<int> loops;
+	set<string> 
 
 	cin>>count;
 
@@ -38,27 +31,35 @@ int main()
 		for (int i = 0; i < 4; ++i)
 			cin.get(ch);
 		if(ch == '1')
-			complexity = 0;
+			est_complexity = 0;
 		else
 		{
 			cin.get();
-			cin.get(ch);
-			complexity = int(ch - '1' + 1);
+			cin.get();
+			cin>>est_complexity;
+			cin.getline(line,10);
 		}
 
+		//test program line by line
 		for(int j = 0;j < lines_per_program)
 		{
-			cin.getline(line,10);
+			cin.getline(line,20);
 
 			if(line[0] == 'E')
 			{
 				if(loops.empty())
 				{
 					cout<<"ERR"<<endl;
+					while(!loops.empty())
+						loops.pop();
 					break;
 				}
 
 				loops.pop();
+			}
+			else
+			{
+
 			}
 
 		}
