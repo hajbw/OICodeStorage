0e206752e7e66defb59352bcf2c433dd7d0942dd
hajbw@ITG2 <hajbw10@126.com>
2018-06-21 18:39:54 +0800

    update luogu P1090(AC)

--- luogu/1090.cpp
+++ luogu/1090.cpp
@@ -2,6 +2,11 @@
 #include <queue>
 #include <vector>
 
+/*
+	P1090 合并果子
+	AC
+*/
+
 using std::cin;
 using std::cout;
 using std::priority_queue;
@@ -10,18 +15,26 @@ using std::vector;
 int main()
 {
 	priority_queue<int,vector<int>,std::greater<int>> que;
-	int n,temp;
+	int n,a,b,ans = 0;
 
 	cin>>n;
 	for (int i = 0; i < n; ++i)
 	{
-		cin>>temp;
-		que.push(temp);
+		cin>>a;
+		que.push(a);
 	}
 
 	while(que.size() != 1)
 	{
-		
+		a = que.top();
+		que.pop();
+		b = que.top();
+		que.pop();
+		ans += a + b;
+		que.push(a + b);
 	}
 
+	cout<<ans;
+
+	return 0;
 }
