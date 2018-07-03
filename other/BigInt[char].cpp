3cebc9134c47a4131d8a7a498ba6f62173d1586d
hajbw@ITG2 <hajbw10@126.com>
2018-07-03 20:10:33 +0800

    add big int

---
+++ other/BigInt[char].cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+struct BigInt
+{
+	static const int MAX_BIT = 1000000;
+
+private:
+	unsigned char data[MAX_BIT];
+
+public:
+	BigInt(long long num)
+	{
+
+	}
+
+	
+};
+
+int main()
+{
+
+}
