f3aaa7a53b8d1afc7c3792bc15465c0f619b4d3e
hajbw@school <hajbw10@126.com>
2018-07-03 18:55:45 +0800

    update luogu P3372

--- luogu/3372.cpp
+++ luogu/3372.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#define l(node) tree[node].left
+#define r(node) tree[node].right
+#define val(node) tree[node].val
+#define del(node) tree[node].delay
+
 /*
 	P3372 【模板】线段树 1
 */
@@ -15,19 +20,69 @@ typedef long long int num;
 struct Node
 {
 	int left,right;
-	num 
-}tree[MAXN << 2 | 2];
+	num val,delay;
+
+	Node()
+	{
+		left = right = 0;
+		val = delay = 0ll;
+	}
+}
+tree[MAXN << 2 | 2];
 
 int M,N;
 
+num arr[MAXN];
 
+void build_tree(int node,int left,int right)
+{
+	l(node) = left;
+	r(node) = right;
+	if(left == right)
+	{
+		val(node) = arr[left];
+		return;
+	}
+	int mid = (left + right) >> 1;
+	build_tree(node << 1,left,mid);
+	build_tree(node << 1 | 1,mid + 1,right);
+	val(node) = val(node << 1) + val(node << 1 | 1);
+}
+
+void add(int node,int left,int right,num k)
+{
+	if()
+}
+
+num query(int node,int left,int right)
+{
+
+}
 
 int main()
 {
-	int x,y;
+	int op_type,x,y;
 	num k;
 
+	cin>>N>>M;
 
+	for(int i = 0;i < N;++i)
+		cin>>arr[i];
 
+	for(int i = 0;i < M;++i)
+	{
+		cin>>op_type;
+		if(op_type == 1)
+		{
+			cin>>x>>y>>k;
+			add(1,x,y,k);
+		}
+		else
+		{
+			cin>>x>>y;
+			cout<<query(1,x,y);
+		}
+	}
 
+	build_tree(1,1,N);
 }
