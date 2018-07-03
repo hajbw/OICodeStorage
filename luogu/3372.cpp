8cd5aaefc8418fc770bd7df3d78e66440536c0cd
hajbw@school <hajbw10@126.com>
2018-07-03 18:12:46 +0800

    new luogu P3372

---
+++ luogu/3372.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+/*
+	P3372 【模板】线段树 1
+*/
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+const int MAXM = 100000,MAXN = 100000;
+
+typedef long long int num;
+
+struct Node
+{
+	int left,right;
+	num 
+}tree[MAXN << 2 | 2];
+
+int M,N;
+
+
+
+int main()
+{
+	int x,y;
+	num k;
+
+
+
+
+}
