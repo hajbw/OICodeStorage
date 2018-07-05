03c1b016085c80a48bee8d2f938338151091b1cb
hajbw@ITG2 <hajbw10@126.com>
2018-07-05 18:22:04 +0800

    add luogu P1037

---
+++ luogu/1037.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <set>
+
+/*
+	P1037 产生数
+*/
+
+using std::cin;
+using std::cout;
+using std::set;
+
+typedef unsigned long long num;
+
+int main()
+{
+	num n;
+	int k,bit,ans = 1,x,y;
+	set<int> bucket[10];
+
+	cin>>n>>k;
+
+	for(int i = 0;i < k;++i)
+	{
+		cin>>x>>y;
+		bucket[x].insert(y);
+	}
+
+	while(n != 0)
+	{
+		bit = n % 10;
+		ans *= bucket[bit].size() + 1;
+		n /= 10;
+	}
+
+	return 0;
+}
