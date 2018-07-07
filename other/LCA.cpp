8ed1eddb9017fd56b5e02c22230abd8e58bd264b
hajbw@school <hajbw10@126.com>
2018-07-07 09:07:27 +0800

    update LCA

--- other/LCA.cpp
+++ other/LCA.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 
-#define l(node) (node<<1)
-#define r(node) (node<<1|1)
+/*
+	P3379【模板】最近公共祖先（LCA）
+*/
+
+#define child(node) (node<<1)
+#define next(node) (node<<1|1)
 #define val(node) (tree[node])
 
+const int MAXN = 500000,MAXM = 500000;
 
-
-const int MAXN = 100
-
-int fa[MAXN],tree[MAXN << 2 | 2];
-
-int LCA(int a,int b)
-{
-
-}
+int fa[MAXN],tree[MAXN << 2 | 2],target_a[MAXM],target_b[MAXM];
+int M,N,S;
 
 int union_find(int x)
 {
 	while(f[x] != f[f[x]])
-		f[x] = f[f[x]];
+		x = f[x] = f[f[x]];
+	return x;
 }
 
 int main()
