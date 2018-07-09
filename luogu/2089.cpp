62f7731afc8bd38db60ca9628d20fd96f3b6b8b5
hajbw@ITG2 <hajbw10@126.com>
2018-07-09 20:27:16 +0800

    update luogu P2089

--- luogu/2089.cpp
+++ luogu/2089.cpp
@@ -1,53 +1,52 @@
-#include <iostream>
+#include <cstdio>
 
 /*
 	P2089 烤鸡
+	AC
 */
 
-using std::cin;
-using std::cout;
-using std::endl;
+using std::printf;
+using std::scanf;
 
 int main()
 {
-	std::ios::sync_with_stdio(false);
 
 	int n,ans = 0;
 
-	cin>>n;
+	scanf("%d",&n);
 
 	if(n < 10||n > 30)
 	{
-		cout<<0<<endl;
+		printf("0");
 		return 0;
 	}
 
-	for(int a = 0;a < 3;++a)
-		for(int b = 0;b < 3;++b)
-			for(int c = 0;c < 3;++c)
-				for(int d = 0;d < 3;++d)
-					for(int e = 0;e < 3;++e)
-						for(int f = 0;f < 3;++f)
-							for(int g = 0;g < 3;++g)
-								for(int h = 0;h < 3;++h)
-									for(int i = 0;i < 3;++i)
-										for(int j = 0;j < 3;++j)
+	for(int a = 1;a <= 3;++a)
+		for(int b = 1;b <= 3;++b)
+			for(int c = 1;c <= 3;++c)
+				for(int d = 1;d <= 3;++d)
+					for(int e = 1;e <= 3;++e)
+						for(int f = 1;f <= 3;++f)
+							for(int g = 1;g <= 3;++g)
+								for(int h = 1;h <= 3;++h)
+									for(int i = 1;i <= 3;++i)
+										for(int j = 1;j <= 3;++j)
 											if(a + b + c + d + e + f + g + h + i + j == n)
 												++ans;
-	cout<<ans<<endl;
+	printf("%d\n",ans);
 
-	for(int a = 0;a < 3;++a)
-		for(int b = 0;b < 3;++b)
-			for(int c = 0;c < 3;++c)
-				for(int d = 0;d < 3;++d)
-					for(int e = 0;e < 3;++e)
-						for(int f = 0;f < 3;++f)
-							for(int g = 0;g < 3;++g)
-								for(int h = 0;h < 3;++h)
-									for(int i = 0;i < 3;++i)
-										for(int j = 0;j < 3;++j)
+	for(int a = 1;a <= 3;++a)
+		for(int b = 1;b <= 3;++b)
+			for(int c = 1;c <= 3;++c)
+				for(int d = 1;d <= 3;++d)
+					for(int e = 1;e <= 3;++e)
+						for(int f = 1;f <= 3;++f)
+							for(int g = 1;g <= 3;++g)
+								for(int h = 1;h <= 3;++h)
+									for(int i = 1;i <= 3;++i)
+										for(int j = 1;j <= 3;++j)
 											if(a + b + c + d + e + f + g + h + i + j == n)
-												cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<" "<<g<<" "<<h<<" "<<i<<" "<<j<<endl;
+												printf("%d %d %d %d %d %d %d %d %d %d\n",a,b,c,d,e,f,g,h,i,j);
 
 	return 0;
 
