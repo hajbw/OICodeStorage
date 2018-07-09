db70f63af6420b63859d6c1ee4b388a911dac997
hajbw@ITG2 <hajbw10@126.com>
2018-07-09 19:46:06 +0800

    add luogu P1149(AC)

---
+++ luogu/1149.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/*
+	P1149 火柴棒等式
+*/
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+	int n,ans[25] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 8, 9, 6, 9, 29, 39, 38, 65, 88, 128};
+
+	cin>>n;
+
+	cout<<ans[n];
+
+	return 0;
+}
---
+++ luogu/1149[ansgen].py
@@ -0,0 +1,29 @@
+#P1149 火柴棒等式
+#answer generator
+
+stick = [6,2,5,5,4,5,6,3,7,6]
+ans = [0,0,0,0,0,0,0,0,0,0]
+ts = [6]
+
+for i in range(1,2002):
+	j = i
+	k = 0
+	while j > 0 :
+		k += stick[j % 10]
+		j //= 10
+	ts.append(k)
+
+print(ts[:20])
+
+#numbers of stick
+for n in range(10,25):
+	curr = 0
+	for i in range(0,1000):
+		for j in range(0,1000):
+			k = i + j
+			if (ts[i] + ts[j] + ts[k]) == (n - 4) :
+				curr += 1
+	ans.append(curr)
+
+print(ans)
+print(ans[14],ans[18])
