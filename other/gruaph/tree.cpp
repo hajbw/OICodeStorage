8e2215a8678e557e1ec71f5b1a47fe14f248cc2e
hajbw@school <hajbw10@126.com>
2018-07-10 17:47:46 +0800

    update tree operator

--- other/gruaph/tree.cpp
+++ other/gruaph/tree.cpp
@@ -11,9 +11,13 @@ const int MAXV = 10005;
 
 vector<int> graph[MAXV];
 
-//get tree diameter
+int root;
 
-//tree dp
+namespace diameter
+{
+
+namespace tree_dp
+{
 
 int max_len[MAXV];
 
@@ -26,7 +30,56 @@ void diameter_tree_dp_dfs(int v)
 	}
 }
 
-//end tree dp
+int get()
+{
+	return 0;
+}
+
+}//end tree dp
+
+namespace find_longest
+{
+
+#ifndef _CSTDIO_
+#include <cstdio>
+#endif
+
+int dist[MAXV];
+
+int dfs_get_furthest(int v,int di)
+/**
+	get furthest from v
+
+	arguments:
+	v : point
+	dist : current distance,initially 0(maybe any int also works)
+
+	return:
+	furthest point from v
+
+*/
+{
+	int furthest = v;//current furthest point
+
+	dist[v] = di;
+
+	for(vector<int>::iterator it = graph[v].begin();it != graph[v].end();++it)
+		if(dis[furthest] < dis[dfs_get_furthest(*it,di + 1)])
+			furthest = *it;
+
+	return furthest;
+}
+	
+int get(int v)
+{
+	int u = dfs_get_furthest(v,0);
+	v = dfs_get_furthest(u,0);
+	return dis[v]
+}
+
+}
+
+}//end diameter
 
 //end get tree diameter
 
@@ -34,7 +87,7 @@ int main()
 {
 	std::ios::sync_with_stdio(false);
 
-	int n,x,y,root = 0;
+	int n,x,y;
 
 	cin>>n>>root;
 
