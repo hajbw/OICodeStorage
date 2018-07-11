e78681a0913f452c5f8b689eca48280875480a73
hajbw@ITG2 <hajbw10@126.com>
2018-07-11 21:24:19 +0800

    update luogu P1008 py ansgen

--- luogu/1008[ansgen].py
+++ luogu/1008[ansgen].py
@@ -1,13 +1,16 @@
 #P1008 三连击
 
 def yes(i):
-	print(i)
 	bit = [0,0,0,0,0,0,0,0,0,0]
+	ans = True
 	for j in [i,i*2,i*3]:
 		bit[j%10] += 1
 		bit[j//10%10] += 1
 		bit[j//100] += 1
-	return bit == [1,1,1,1,1,1,1,1,1,1]
+	for k in bit:
+		if k != 1:
+			ans = False
+	return ans
 
 
 for i in range(100,333):
