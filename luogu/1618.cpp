88e4ebffedd1a7a2d1fc3cf4dfbe9da95b7f2b1f
hajbw@ITG2 <hajbw10@126.com>
2018-07-11 22:08:38 +0800

    update luogu P1618

--- luogu/1618.cpp
+++ luogu/1618.cpp
@@ -3,33 +3,39 @@
 
 /*
 	P1618 三连击（升级版）
+	
 */
 
-int alphabet[10];
+int alphabet[20];
 
 int satisfy(int i,int n,int m)
 {
 	std::memset(alphabet,0,sizeof(alphabet));
-	++alphabet[i/100];
+	int res = 1;
+	++alphabet[i/100%10];
 	++alphabet[i/10%10];
 	++alphabet[i%10];
-	++alphabet[n/100];
+	++alphabet[n/100%10];
 	++alphabet[n/10%10];
 	++alphabet[n%10];
-	++alphabet[m/100];
+	++alphabet[m/100%10];
 	++alphabet[m/10%10];
 	++alphabet[m%10];
+	for(int i = 1;i < 10;++i)
+		if(alphabet[i] != 1)
+			res = 0;
+	return res;
 }
 
 int main()
 {
 	int A,B,C;
 
-	std::scanf("%d %d %d",A,B,C);
+	std::scanf("%d %d %d",&A,&B,&C);
 
-	for(int i = 100;i <= 1000*A*C;++i)
-		if(satisfy(i,i*B/A,i*C/A))
-			std::printf("%d %d %d",i,i*B/A,i*C/A);
+	for(int i = 1;i*C < 1000;++i)
+		if(i*A > 100 && satisfy(i*A,i*B,i*C))
+			std::printf("%d %d %d\n",i*A,i*B,i*C);
 
 	return 0;
 }
