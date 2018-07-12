2e0497ff2202a469ce562a9985f4f6a1e09310ef
hajbw@ITG2 <hajbw10@126.com>
2018-07-12 08:39:49 +0800

    add luogu P3958

---
+++ luogu/3958.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+
+/*
+	P3958 奶酪
+*/
+
+using std::printf;
+using std::scanf;
+
+const int MAXN = 1005;
+
+struct hole
+{
+	int x,y,z;
+}
+holes[MAXN];
+
+inline void read(int &x,int &y,int &z)
+{
+
+}
+
+inline int touched(hole &a,hole &b)
+{
+
+}
+
+int main()
+{
+	int T,n,h,r;
+
+	scanf("")
+	return 0;
+}
