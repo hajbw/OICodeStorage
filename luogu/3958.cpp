2ea98e3ad39a27e1b598d18d10e8b1131c1de4d8
hajbw@school <hajbw10@126.com>
2018-07-12 09:51:08 +0800

    update luogu P3958

--- luogu/3958.cpp
+++ luogu/3958.cpp
@@ -15,11 +15,54 @@ struct hole
 }
 holes[MAXN];
 
-char buffer[40];
-inline void read(int &x,int &y,int &z)
+char buf[40];
+template<class T> void read(T &x,T &y,T &z)
 {
-	cin.getline(buffer,40);
+	x = y = z = 0;
+
+	char *ptr = buf;
+	cin.getline(buf,40);
+	int flag = 0;
+	while(*ptr < '0' || *ptr > '9')
+	{
+		flag ^= (*ptr == '-');
+		++ptr;
+	}
+	while(*ptr >= '0' && *ptr <= '9')
+	{
+		x = (x<<1) + (x<<3) + (*ptr-'0')//2x+8x+int(*ptr)
+		++ptr;
+	}
+	if(flag)
+		x = ~x + 1;
+	flag = 0;
+	while(*ptr < '0' || *ptr > '9')
+	{
+		flag ^= (*ptr == '-');
+		++ptr;
+	}
+	while(*ptr >= '0' && *ptr <= '9')
+	{
+		y = (y<<1) + (y<<3) + (*ptr-'0')//2y+8y+int(*ptr)
+		++ptr;
+	}
+	if(flag)
+		y = ~y + 1;
+	flag = 0;
+	while(*ptr < '0' || *ptr > '9')
+	{
+		flag ^= (*ptr == '-');
+		++ptr;
+	}
+	while(*ptr >= '0' && *ptr <= '9')
+	{
+		z = (z<<1) + (z<<3) + (*ptr-'0')//2z+8z+int(*ptr)
+		++ptr;
+	}
+	if(flag)
+		z = ~z + 1;
 }
+
 
 inline int touched(hole &a,hole &b)
 {
