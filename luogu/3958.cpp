ee98c22bf2b60fa5e386909f97b55e0e89a63a6d
hajbw@school <hajbw10@126.com>
2018-07-12 09:08:30 +0800

    update luogu P3958

--- luogu/3958.cpp
+++ luogu/3958.cpp
@@ -1,11 +1,11 @@
-#include <cstdio>
+#include <iostream>
 
-/*
+/**
 	P3958 奶酪
 */
 
-using std::printf;
-using std::scanf;
+using std::cin;
+using std::cout;
 
 const int MAXN = 1005;
 
@@ -15,9 +15,10 @@ struct hole
 }
 holes[MAXN];
 
+char buffer[40];
 inline void read(int &x,int &y,int &z)
 {
-
+	cin.getline(buffer,40);
 }
 
 inline int touched(hole &a,hole &b)
