637a8eae6afb389e3726fc653914b22be8919a43
hajbw@school <hajbw10@126.com>
2018-07-12 09:08:51 +0800

    add luogu P1262

---
+++ luogu/1262.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <vector>
+
+#ifndef max
+#define max(a,b) (a > b ? a : b)
+#endif
+
+#ifndef min
+#define min(a,b) (a < b ? a : b)
+#endif
+
+using std::scanf;
+using std::printf;
+
+/**
+	P1262 间谍网络
+*/
+
+using std::vector;
+
+const int MAXV = 3005,MAXE = 12000;
+
+vector<int> graph[MAXV];
+int cost[MAXV];//cost to buy.unable if 0
+int n,p,r;//... ntr???
+
+int jzm_clock,timestamp[MAXV];
+void tarjan(int v)
+{
+
+}
+
+int main()
+{
+	int x,y;
+
+	scanf("%d",&n);
+	scanf("%d",&p);
+	for(int i = 0;i < p;++i)
+	{
+		scanf("%d %d",&x,&y);
+		cost[x] = y;
+	}
+	scanf("%d",&r);
+	for(int i = 0;i < r;++i)
+	{
+		scanf("%d %d",&x,&y);
+		graph[x].push_back(y);
+	}		
+
+	return 0;
+}
