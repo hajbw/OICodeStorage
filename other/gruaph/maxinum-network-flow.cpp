c314f72343ab88229383e1b3cbf5680587e3e093
hajbw@school <hajbw10@126.com>
2018-07-13 09:43:26 +0800

    update ntwk-flow

--- other/gruaph/maxinum-network-flow.cpp
+++ other/gruaph/maxinum-network-flow.cpp
@@ -9,6 +9,8 @@
 #define max(a,b) (a > b ? a : b)
 #endif
 
+#define rev(edg_index) (edg_index ^ 1)
+
 /**
 	template:maxinum-network-flow
 	also passing luogu P3376
@@ -17,20 +19,74 @@
 using std::cin;
 using std::cout;
 
-const int MAXV = 10005,MAXE = 100010;
+const int MAXV = 10005,MAXE = 100010,READ_MAX_LINE_LENTH = 40;
 
 struct Edge
 {
-	int to,next,cap,flow;
+	int to,next,cap;
 }
-edges[MAXE],rev_edges[MAXE];
+edges[MAXE << 1];
+
+/*
+	NOTE:
+		index(edge) % 2 == 0
+		index(rev_edge) % 2 == 1
+*/
 
 int
 	V,E,S,T,//Vertices,Edges,Start point,Terminate point
 	edge_index,
 	head[MAXV],
-	rev_head[MAXV],
 	visited[MAXV];
+
+
+char buf[READ_MAX_LINE_LENTH];
+template<class T> void read(T &x,T &y,T &z)
+{
+	x = y = z = 0;
+
+	char *ptr = buf;
+	cin.getline(buf,READ_MAX_LINE_LENTH);
+	int flag = 0;
+	while(*ptr < '0' || *ptr > '9')
+	{
+		flag ^= (*ptr == '-');
+		++ptr;
+	}
+	while(*ptr >= '0' && *ptr <= '9')
+	{
+		x = (x<<1) + (x<<3) + (*ptr-'0');//2x+8x+int(*ptr)
+		++ptr;
+	}
+	if(flag)
+		x = ~x + 1;
+	flag = 0;
+	while(*ptr < '0' || *ptr > '9')
+	{
+		flag ^= (*ptr == '-');
+		++ptr;
+	}
+	while(*ptr >= '0' && *ptr <= '9')
+	{
+		y = (y<<1) + (y<<3) + (*ptr-'0');//2y+8y+int(*ptr)
+		++ptr;
+	}
+	if(flag)
+		y = ~y + 1;
+	flag = 0;
+	while(*ptr < '0' || *ptr > '9')
+	{
+		flag ^= (*ptr == '-');
+		++ptr;
+	}
+	while(*ptr >= '0' && *ptr <= '9')
+	{
+		z = (z<<1) + (z<<3) + (*ptr-'0');//2z+8z+int(*ptr)
+		++ptr;
+	}
+	if(flag)
+		z = ~z + 1;
+}
 
 void addedge(int u,int v,int cap)
 {
@@ -39,23 +95,11 @@ void addedge(int u,int v,int cap)
 	edges[edge_index].cap = cap;
 	head[u] = edge_index;
 	++edge_index;
-	rev_edges[edge_index].to = v;
-	rev_edges[edge_index].next = rev_head[u];
-	rev_edges[edge_index].cap = cap;
-	rev_head[v] = edge_index;
-}
-
-int* f(int u,int v)
-/**
-	adapter for sgq's f[][] (flow martix)
-	hope it would never be useful
-*/
-{
-	int edge = head[u];
-	for(;edge != 0;edge = edges[edge].next)
-		if(edges[edge].to = v)
-			break;
-	return edge ? &edges[edge].flow : (int*)0;
+	edges[edge_index].to = u;
+	edges[edge_index].next = head[v];
+	edges[edge_index].cap = 0;
+	head[v] = edge_index;
+	++edge_index;
 }
 
 int dfs(int u,int amount)
@@ -78,26 +122,15 @@ int dfs(int u,int amount)
 
 	for(int edge = head[u];edge != 0;edge = edges[edge].next)
 	{
-		if(!visited[edges[edge].to] && edges[edge].cap > edges[edge].flow)
+		if(!visited[edges[edge].to] &&
+			edges[edge].cap > 0)
 		{
-			min_ = min(amount,edges[edge].cap - edges[edge].flow);
+			min_ = min(amount,edges[edge].cap);
 			t = dfs(edges[edge].to,min_);
 			if(t)
 			{
-				edges[edge].flow += t;
-				return t;
-			}
-		}
-	}
-	for(int edge = rev_head[u];edge != 0;edge = edges[edge].next)
-	{
-		if(!visited[edges[edge].to] && edges[edge].flow > 0)
-		{
-			min_ = min(amount,edges[edge].flow);
-			t = dfs(edges[edge].to,min_);
-			if(t)
-			{
-				edges[edge].flow -= t;
+				edges[edge].cap -= t;
+				edges[rev(edge)].cap += t;
 				return t;
 			}
 		}
