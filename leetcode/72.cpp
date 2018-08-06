f2da34467757d1ce0fa1b7728fde3c20637b749f
hajbw@QB <hajbw10@126.com>
2018-08-06 19:39:10 +0800

    update leetcode#72

--- leetcode/72.cpp
+++ leetcode/72.cpp
@@ -1,10 +1,53 @@
 //72. Edit Distance
 
-class Solution {
-	const int MAXN = 10010;
+#ifdef min
+#undef min
+#endif
+#define min(a,b) (a < b ? a : b)
+
+#ifdef max
+#undef max
+#endif
+#define max(a,b) (a > b ? a : b)
+
+#ifndef min3
+#define min3(a,b,c) (a < b ? (a < c ? a : c) : (b < c ? b : c))
+#endif
+
+class Solution
+{
+	const int MAXN = 20010;
 public:
-    int minDistance(string word1, string word2) {
-        int *dp = new int[MAXN];
-        
+    int minDistance(string word1, string word2)
+    {
+        int
+        	*temp,
+        	*dp1 = new int[MAXN],//current,dp1[j] = dp[i][j]
+        	*dp2 = new int[MAXN];//last,dp2[j] = dp[i-1][j]
+
+        memset(dp1,0,min(word1.size(),word2.size()) * sizeof(int))
+        for(int i = 0;i < MAXN;++i)
+        	dp2[i] = i;
+
+        for(int i = 1;i < word1.size();++i)
+        {
+        	dp1[0] = i;
+
+        	for(int j = 1;j < word2.size();++j)
+        	{
+        		if(word1[i] == word2[j])
+        			dp1[j] = dp2[j - 1];
+        		else
+        			dp1[j] = 
+        	}
+
+        	temp = dp1;
+        	dp1 = dp2;
+        	dp2 = temp;
+        }
+
+        //delete dp1;
+        //delete dp2;
+        return 0;
     }
 };
