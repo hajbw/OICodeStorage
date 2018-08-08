7e715db82e2ac7060e5c741a493580f7b372f9c6
hajbw@QB <hajbw10@126.com>
2018-08-08 13:44:31 +0800

    add a bool array template

---
+++ other/bool_array.cpp
@@ -0,0 +1,72 @@
+/**
+	bool array template
+*/
+
+//class version
+
+#ifndef _GLIBCXX_CSRTING
+#include <cstring>
+#endif
+
+class bool_array
+{
+	unsigned *data,data_size;
+
+public:
+
+	static const int DEFAULT_SIZE = 16 * sizeof(int);
+
+	class bool_array_operator
+	{
+		bool_array *obj;
+		unsigned index,offsset;
+
+		bool_array_operator(bool_array *obj,unsigned index)
+		{
+			bool_array_operator(obj,index / sizeof(unsigned),index % sizeof(unsigned));
+		}
+
+		bool_array_operator(bool_array *obj,unsigned index,unsigned offset)
+		{
+			this->index = index;
+			this->offset = offset;
+			this->obj = obj;
+		}
+
+
+	}
+
+	bool_array(unsigned n = DEFAULT_SIZE)
+	{
+		data_size = (n + sizeof(unsigned) - 1) / sizeof(unsigned);
+		data = new unsigned[data_size];
+		std::memset(data,0,sizeof(data));
+	}
+
+	bool_array(bool arr[],unsigned n)
+	{
+		bool_array(n);
+		for(int i = 0;i < n;++i)
+			data[i / sizeof(unsigned)] |= 1 << (index % sizeof(unsigned));
+	}
+
+	~bool_array()
+	{
+		delete[] data;
+	}
+
+	bool operator[](unsigned index)
+	{
+		return data[index / sizeof(unsigned)] & (1 << (index % sizeof(unsigned)));
+	}
+
+	bool_array_operator operator[](unsigned index)
+	{
+		return new bool_array_operator(this,index);
+	}
+
+	int size()
+	{
+		return data_size * sizeof(unsigned);
+	}
+};
