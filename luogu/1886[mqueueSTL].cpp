3c80c5eb82a0aada2a38825560eca0758e9ba02f
hajbw@his-hasee <hajbw10@126.com>
2018-08-20 18:02:29 +0800

    update luogu P1886

--- luogu/1886[mqueueSTL].cpp
+++ luogu/1886[mqueueSTL].cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <queue>
+#include <deque>
 
 /**
 	P1886 滑动窗口
@@ -9,21 +9,19 @@
 
 using std::cin;
 using std::cout;
-using std::queue;
+using std::deque;
 
 const int MAXN = 1000010;
 
+struct hpair
+{
+	int value,index;
+
+	hpair():value(),index(){}
+	hpair(int v,int i):value(v),index(i){}
+};
+
 int num[MAXN],N,K;
-
-void get_min()
-{
-
-}
-
-void get_max()
-{
-
-}
 
 template<class T>void read(T &x)
 {
@@ -32,6 +30,44 @@ template<class T>void read(T &x)
 	while(ch < '0' || ch > '9'){flag ^= (ch == '-');in.get(ch);}
 	while(ch >= '0' && ch <= '9'){x = (x<<1) + (x<<3) + (ch-'0');in.get(ch);}
 	if(flag)x = -x;
+}
+
+void get_min()
+{
+	deque<hpair> quq;
+
+	for(int i = 0;i < N;++i)
+	{
+		//push in
+		while(quq.back().value > num[i] && !quq.empty())
+			quq.pop_back();
+		quq.push_back(hpair);
+
+		if(quq.front().index > i - K)
+			quq.pop_head();
+
+		if(i > K - 2)
+			cout<<quq.front().value<<" ";
+	}
+}
+
+void get_max()
+{
+	deque<hpair> quq;
+
+	for(int i = 0;i < N;++i)
+	{
+		//push in
+		while(quq.back().value < num[i] && !quq.empty())
+			quq.pop_back();
+		quq.push_back(hpair);
+
+		if(quq.front().index > i - K)
+			quq.pop_head();
+
+		if(i > K - 2)
+			cout<<quq.front().value<<" ";
+	}
 }
 
 int main()
