a7d89a8d31ff9645938027d8d07baf04282ae26b
hajbw@his-hasee <hajbw10@126.com>
2018-08-20 20:02:59 +0800

    add nnszoj contest 1 problem 3(AC)

---
+++ nnszoj/#22.cpp
@@ -0,0 +1,25 @@
+#define DEBUG 1
+
+#include <iostream>
+#include <cstdio>
+
+/**
+	contest 1 problem 1 #22
+	A. 吉祥数(ghillie)
+*/
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+#if DEBUG
+
+#else
+	freopen("ghillie.in","r",stdin);
+	freopen("ghillie.out","r",stdout);
+#endif
+
+	cin>>N;
+	
+}
