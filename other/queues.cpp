a6e6e128a383f922ce7093f725b4f6e7eabf7f99
hajbw@his-hasee <hajbw10@126.com>
2018-08-22 22:25:34 +0800

    update queue templates quq

--- other/queues.cpp
+++ other/queues.cpp
@@ -1,4 +1,7 @@
-//queue
+#include <iostream>
+
+using std::cin;
+using std::cout;
 
 template<class T>class hdeque
 {
@@ -11,63 +14,55 @@ template<class T>class hdeque
 		node(T &d,node *p,node *n):data(d),prev(p),next(n){}
 	};
 
-	node head,tail,*temp;
-	int size_;
+	node *head,*tail;
+	size_t size_;
 
 public:
 
 	hdeque()
 	{
-		head.next = &tail;
-		tail.prev = &head;
+		head = tail = NULL;
 		size_ = 0;
 	}
 
 	T& front()
 	{
-		return *head.next;
+		return head->data;
 	}
 
 	T& back()
 	{
-		return *tail.prev;
+		return tail->data;
 	}
 
 	void push_front(T &val)
 	{
-		temp = new node(val,&head,head.next);
-		head.next = temp;
-		temp->next->prev = temp;
+		head = new node(val,NULL,head);
+		if(!tail)
 		++size_;
 	}
 
 	void push_back(T &val)
 	{
-		temp = new node(val,tail.prev,&tail);
-		tail.prev = temp;
-		temp->prev->next = temp;
+		tail = new node(val,tail,NULL);
 		++size_;
 	}
 
 	void pop_front()
 	{
-		if(head.next == &tail)
+		if(!head)
 			return;
-		temp = head.next;
-		head.next = temp->next;
-		temp->next->prev = &head;
-		delete temp;
+		node *temp = head->next;
+		delete head;
+		head = temp;
 		--size_;
 	}
 
 	void pop_back()
 	{
-		if(tail.prev == &head)
+		if(!tail)
 			return;
-		temp = tail.prev;
-		tail.prev = temp->prev;
-		temp->prev->next = &tail;
-		delete temp;
+		
 		--size_;
 	}
 
@@ -80,10 +75,19 @@ public:
 	{
 		return !size_;
 	}
+
+	friend std::ostream& operator<<(std::ostream &out,const hdeque &quq)
+	{
+		for(node *ptr = quq.head;ptr;ptr = ptr->next)
+			cout<<ptr->data;
+		return out;
+	}
 	
 };
 
+
 int main()
 {
+
 	return 0;
 }
