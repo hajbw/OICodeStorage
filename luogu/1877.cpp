a2c061ae5e7da3fecdf8ad156ac96e10e9a6e4b0
hajbw@his-hasee <hajbw10@126.com>
2018-08-25 16:30:36 +0800

    update luogu P1877

--- luogu/1877.cpp
+++ luogu/1877.cpp
@@ -3,7 +3,7 @@
 #define min(a,b) ((a) < (b) ? (a) : (b))
 #define max(a,b) ((a) > (b) ? (a) : (b))
 
-#define DEBUG 1
+#define DEBUG 0
 
 #if DEBUG
 #include <windows.h>
@@ -11,6 +11,7 @@
 
 /**
 	P1877 [HAOI2012]音量调节
+	AC
 */
 
 using std::cin;
@@ -23,6 +24,7 @@ int levels[MAXN],reachable[MAXN][MAXL];
 void read(int &x)
 {
 	char ch = '\0';x = 0;
+	cin.get(ch);
 	while(ch < '0' || ch > '9')cin.get(ch);
 	while(ch >= '0' && ch <= '9'){x = (x<<1) + (x<<3) + ch - '0';cin.get(ch);}
 }
@@ -37,23 +39,21 @@ int main()
 	for(int i = 1;i <= N;++i)
 		read(levels[i]);
 
-	for (int i = 0; i < N; ++i)
-	{
-		cout<<levels[i];
-	}
-
 	reachable[0][beginlevel] = 1;
 	for(int i = 1;i <= N && !flag;++i)
 	{
 		flag = 1;
-		for(int j = 0;j <= maxlevel - levels[i];++j)
+		for(int j = 0;j <= maxlevel;++j)
 		{
 			if(reachable[i - 1][j])
-				reachable[i][j + levels[i]] = 1,
-				flag = 0,
-				cout<<i<<" , "<<j<<" reachable.\n";
+			{
+				if(j + levels[i] <= maxlevel)	reachable[i][j + levels[i]] = 1;
+				if(j >= levels[i])				reachable[i][j - levels[i]] = 1;
+				flag = 0;
+			}
 		}
-
+		if(flag)
+			break;
 	}
 
 	if(flag)
@@ -62,11 +62,18 @@ int main()
 	{
 		int i;
 		for(i = maxlevel;i && !reachable[N][i];--i);
-		cout<<i;
+		cout<<(i ? i : -1);//quite ugly fix
 	}
 
 #if DEBUG
 	cout<<"\nflag = "<<flag<<"\n";
+
+	for(int i = 0;i <= N;++i)
+	{
+		for(int j = 0;j <= maxlevel;++j)
+			cout<<reachable[i][j];
+		cout<<"\n";
+	}
 
 	system("pause");
 #endif
