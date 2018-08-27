e580dc55bc91bcdb8e43a4071f90cfc12203a197
hajbw@his-hasee <hajbw10@126.com>
2018-08-27 21:15:57 +0800

    update luogu problems in contest

--- luogu/T43900.cpp
+++ luogu/T43900.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+#include <queue>
+
+#define lch(n)	(n<<1)
+#define rch(n)	(n<<1|1)
+#define fa(n)	(n>>1)
+
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+#define DEBUG 1
 
 /**
 	T43900 淘汰赛制(elimination.pas/c/cpp)
@@ -6,10 +16,20 @@
 
 using std::cin;
 using std::cout;
+using std::queue;
 
-const int MAXN = 11;
+const int MAXN = 10;
 
-int N,p[MAXN][MAXN],posibility[1<<MAXN];
+struct hpair
+{
+	long long possibility;
+	int index;
+
+	hpair():possibility(1ll),index(){}
+};
+
+int p[1<<MAXN|1][1<<MAXN|1];
+hpair tree[1<<(MAXN+1)|3];
 
 template<class T> void read(T &x,std::istream &in = std::cin)
 {
@@ -21,13 +41,47 @@ template<class T> void read(T &x,std::istream &in = std::cin)
 
 int main()
 {
-	read(N);
+	//std::ios::sync_with_stdio(false);
 
-	for(int i = 0;i < (1<<N);++i)
-		for(int j = 0;j < (1<<N);++j)
+	int N,cnt;
+
+	read(N);
+	cnt = 1 << N;
+
+	for(int i = 1;i <= cnt;++i)
+		for(int j = 1;j <= cnt;++j)
 			read(p[i][j]);
 
-	
+	//initialize
+	for(int i = cnt;i < (cnt<<1|1);++i)
+		tree[i].index = i - cnt;
+
+	//then opreate on a fucking perfect bin tree
+	for(int i = cnt>>1;i;i >>= 1)
+	{
+		for(int j = i;j < i << 1;++j)
+		{
+			tree[lch(j)].possibility *= p[tree[lch(j)].index][tree[rch(j)].index];
+			tree[rch(j)].possibility *= p[tree[rch(j)].index][tree[lch(j)].index];
+			if(tree[lch(j)].possibility >= tree[rch(j)].possibility)
+				tree[j] = tree[lch(j)];
+			else
+				tree[j] = tree[rch(j)];
+		}
+	}
+
+#if DEBUG
+
+	for(int i = 0;i < cnt<<1;++i)
+		cout<<tree[i].index<<" "<<tree[i].possibility<<"\n";
+	cout<<"\n";
+
+	system("pause");
+#else
+
+	cout<<tree[1].index;
+
+#endif
 
 	return 0;
 }
--- luogu/T43901.cpp
+++ luogu/T43901.cpp
@@ -7,16 +7,34 @@
 using std::cin;
 using std::cout;
 
+const int P = 1000;
+
 template<class T> void read(T &x,std::istream &in = std::cin)
 {
-	char ch = '\0';int flag = 0;x = 0;
+	char ch = '\0';x = 0;
 	in.get(ch);
-	while(ch < '0' || ch > '9'){flag ^= (ch == '-');in.get(ch);}
+	while(ch < '0' || ch > '9')in.get(ch);
 	while(ch >= '0' && ch <= '9'){x = (x<<1) + (x<<3) + (ch-'0');in.get(ch);}
-	if(flag)x = -x;
+}
+
+int fast_pow_mod(int a,int b)
+{
+	int result = 1;
+	while(b)
+	{
+		if(b & 1)
+			result = result * a % P;
+		a = a * a % P;
+		b >>= 1;
+	}
+	return result;
 }
 
 int main()
 {
+	int K,X;
+
+	read(K);read(X);
+
 	return 0;
 }
