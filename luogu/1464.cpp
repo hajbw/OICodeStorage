a100aff322e95a7f0ae31dbac192c14e63282335
hajbw@his-hasee <hajbw10@126.com>
2018-08-30 19:28:46 +0800

    update luogu P1464(AC)

--- luogu/1464.cpp
+++ luogu/1464.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 /**
-
+	P1464 Function
+	AC
 */
 
 using std::cin;
@@ -9,16 +10,7 @@ using std::cout;
 
 long long w[22][22][22];
 
-template<class T> void read(T &x,std::istream &in = std::cin)
-{
-	char ch = '\0';int flag = 0;x = 0;
-	in.get(ch);
-	while(ch < '0' || ch > '9'){flag ^= (ch == '-');in.get(ch);}
-	while(ch >= '0' && ch <= '9'){x = (x<<1) + (x<<3) + (ch-'0');in.get(ch);}
-	if(flag)x = -x;
-}
-
-long long dfs(const long &a,const long &b,const long &c)
+long long dfs(const long long &a,const long long &b,const long long &c)
 {
 	if(a <= 0 || b <= 0 || c <= 0)
 		return 1ll;
@@ -29,20 +21,23 @@ long long dfs(const long &a,const long &b,const long &c)
 		return w[a][b][c];
 
 	if(a < b && b < c)
-		w[a][b][c] = dfs(a,b,c - 1ll) + dfs(a,b - 1ll,c - 1ll) - dfs(a,b - 1ll,c);
+		return w[a][b][c] = dfs(a,b,c - 1ll) + dfs(a,b - 1ll,c - 1ll) - dfs(a,b - 1ll,c);
 
 	return w[a][b][c] = dfs(a - 1ll,b,c) + dfs(a - 1ll,b - 1ll,c) + dfs(a - 1ll,b,c - 1ll) - dfs(a - 1ll,b - 1ll,c - 1ll);
 }
 
 int main()
 {
+	std::ios::sync_with_stdio(false);
+
 	long long a,b,c;
 
-	
+	for(int i = 1;i < 21;++i)
+		w[i][i][i] = 1<<i;
 
 	while( ~(1 ^ 1) )
 	{
-		read(a);read(b);read(c);
+		cin>>a>>b>>c;
 
 		if(a == -1 && b == -1 && c == -1)
 			break;
