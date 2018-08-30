f612987a5605a72953e87931aca0d35ce0ce4b67
hajbw10@ITG3 <hajbw10@126.com>
2018-08-30 18:01:30 +0800

    update luogu P2661

--- luogu/2661.cpp
+++ luogu/2661.cpp
@@ -3,6 +3,8 @@
 
 #define min(a,b) (a < b ? a : b)
 
+#define DEBUG 1
+
 /*
 	P2661 信息传递
 */
@@ -16,18 +18,21 @@ int
 	N,jzm_clock = 0,ans = 19260817,
 	graph[MAXN],dfn[MAXN],low[MAXN];
 
-stack<int> st;
+std::stack<int> st;
 
 void read(int &x)
 {
 	char ch = '\0';x = 0;
 	cin.get(ch);
 	while(ch < '0' || ch > '9')cin.get(ch);
-	while(ch >= '0' && ch <= '9'){x = (x<<1) + (x<<3) + ch - '0';}
+	while(ch >= '0' && ch <= '9'){x = (x<<1) + (x<<3) + ch - '0';cin.get(ch);}
 }
 
 void tarjan(int u)
 {
+#if DEBUG
+	cout<<u<<" ";
+#endif
 	int v = graph[u];
 	dfn[u] = low[u] = ++jzm_clock;
 	st.push(u);
@@ -40,34 +45,45 @@ void tarjan(int u)
 	else
 		low[u] = min(low[u],dfn[v]);
 
-	if(low[u] = dfn[u])
+	if(low[u] == dfn[u])
 	{
 		int count = 0;
-		do
+		while(v != u)
 		{
 			v = st.top();st.pop();
 			++count;
 		}
-		while(v != u);
-		if(count > 1)
-			ans = min(ans,count);
+		if(count > 0 && count < ans)
+			ans = count;
 	}
 }
 
 int main()
 {
-	std::ios::sync_with_stdio(false);
+	//std::ios::sync_with_stdio(false);
 
 	read(N);
 
 	for(int i = 0;i < N;++i)
-		read(to[i]);
+		read(graph[i]);
+
+#if DEBUG
+		for(int i = 0;i < N;++i)
+			cout<<graph[i]<<" ";
+		cout<<"\n";
+#endif
 
 	for(int i = 0;i < N;++i)
 		if(!dfn[i])
 			tarjan(i);
 
 	cout<<ans;
+
+#if DEBUG
+
+	system("pause");
+
+#endif
 
 	return 0;
 }
