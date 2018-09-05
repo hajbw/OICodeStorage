368522fddbb4c4f59b7f89cc1191812a7c7b0aa3
hajbw@his-hasee <hajbw10@126.com>
2018-09-05 12:48:50 +0800

    update luogu P1002 P1015

--- luogu/1002.cpp
+++ luogu/1002.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 #define min(a,b) ((a) < (b) ? (a) : (b))
 #define max(a,b) ((a) > (b) ? (a) : (b))
@@ -14,40 +15,74 @@ const int MAXN = 22,MAXM = 22,MAXBIT = 50;
 
 struct hint
 /**
-	fuck high precisions.q
+	fuck high precisions.
 */
 {
 	char ch[MAXBIT];
 	int index;//first that doesn't exist aka. index - 1 exist
 
 	hint():ch(),index(0){}
-	hint(int &i):ch(),index(i){}
+
+	hint& operator=(int a)
+	{
+		index = 0;
+
+		while(a)
+		{
+			ch[index++] = a % 10;
+			a /= 10;
+		}
+
+		return *this;
+	}
 
 	hint& operator+=(hint &a)
 	{
-		char carry = 0;
-		for(int i = 0;i < )
+		char car = 0;
+		for(int i = 0;i <= max(index,a.index);++i)
+		{
+			ch[i] += a.ch[i] + car;
+			car = ch[i] / 10;
+			ch[i] %= 10; 
+		}
+		return *this;
 	}
-
-
-}
+	
+	friend std::ostream& operator<<(std::ostream &out,hint &a)
+	{
+		for(int i = a.index;i;--i)
+			out.put(a.ch[i - 1] + '0');
+		return out;
+	}
+};
 
 hint f[MAXN][MAXM];
 
 int main()
 {
-	int M,N,horse_x,horse_y;
+	int M,N,horse_x,horse_y,i,j;
 
 	cin>>N>>M>>horse_x>>horse_y;
 
-	for(i = 0;i < MAXM;++i)
-	{
+	f[0][0] = 1;
 
+	for(i = 1;i <= MAXM;++i)
+	{
+		if(!horse_x && horse_y == i)
+			break;
+		f[0][i] = f[0][i - 1];
 	}
 
-	for(i = 1;i < MAXN;++i)
+	for(i = 1;i <= MAXN;++i)
 	{
-		for(j = 1;j < MAXM;++j)
+		if(!horse_y && horse_x == i)
+			break;
+		f[i][0] = f[i - 1][0];
+	}
+
+	for(i = 1;i <= MAXN;++i)
+	{
+		for(j = 1;j <= MAXM;++j)
 		{
 
 		}
--- luogu/1015.cpp
+++ luogu/1015.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define DEBUG 1
+
 /**
 	P1015 回文数
 */
@@ -7,19 +9,82 @@
 using std::cin;
 using std::cout;
 
-const int MAXN = 16,MAXM = 101;
-const char bitlist[MAXN] = {'0','1','2','3','4','5','6','7','8','9'};
+const int MAXN = 16,MAXM = 120;
+const char bitlist[MAXN] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 
-char M[MAXM];
+int N,iM;
+char M[MAXM],Mbuf[MAXM],revM[MAXM];
 
-void readN()
+void readM()
 {
-	char ch = '\0';
-	while(ch < '0' || ch > '9')cin.get(ch);
+	cin.getline(Mbuf,MAXM);
+	cin.getline(Mbuf,MAXM);
+	for(char *ch = Mbuf;*ch;++ch)
+	{
+		if(DEBUG)cout.put(*ch);
+		//to number
+		if(*ch >= '0' && *ch <= '9')
+			*ch -= '0';
+		else if(*ch >= 'A' && *ch <= 'Z')
+			*ch -= 'A' + '0';
+		else if(*ch >= 'a' && *ch <= 'z')
+			*ch -= 'a' + '0';
+		else
+			continue;
+		if(DEBUG)cout.put(*ch);
+
+		M[iM++] = *ch;
+	}
+#if DEBUG
+	cout<<Mbuf<<" ";
+	for(int i = 0;i < iM;++i)
+		cout<<M[i] + '0'<<'-';
+	cout<<" "<<iM<<"\n\n";
+#endif
+}
+
+bool verify()
+{
+	for(int l = 0,r = iM - 1;l < r;++l,--r)
+		if(M[l] != M[r])
+			return false;
+
+	return true;
+}
+
+void opterate()
+{
+	int i,car;
+
+	//copy to revM
+	for(i = 0;i < iM;++i)
+		revM[iM - i - 1] = M[i];
+
+	//'add' operation
+	for(i = 0;i < iM;++i)
+	{
+		M[i] += revM[i] + car;
+		car = M[i] / N;
+		M[i] %= N;
+	}
+	if(car)
+		M[iM++] = car;
 }
 
 int main()
 {
+	int count = 0;
+
 	cin>>N;
-	readN();
+	readM();
+
+	while(++count < 32 && verify())
+		opterate();
+
+	if(count > 30)
+		cout<<"Impossible!";
+	else
+		cout<<"STEP="<<count;
+
+	return 0;
 }
