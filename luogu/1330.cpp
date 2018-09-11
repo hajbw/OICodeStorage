ee9f7733c733ca6df42ac08266b18b4ecd950bff
hajbw@his-hasee <hajbw10@126.com>
2018-09-11 21:46:10 +0800

    update luogu P1330

--- luogu/1330.cpp
+++ luogu/1330.cpp
@@ -40,14 +40,18 @@ inline void read(int &a,int &b)
 	a = b = 0;
 	cin.getline(buf,49);
 	ch = buf;
-	while(*ch && *ch < '0' || *ch > '9')++ch;
+	while(*ch && (*ch < '0' || *ch > '9'))++ch;
 	while(*ch >= '0' && *ch <= '9'){a = (a<<1) + (a<<3) + *ch - '0';++ch;}
-	while(*ch && *ch < '0' || *ch > '9')++ch;
+	while(*ch && (*ch < '0' || *ch > '9'))++ch;
 	while(*ch >= '0' && *ch <= '9'){b = (b<<1) + (b<<3) + *ch - '0';++ch;}
 }
 
 void dfs(const int &u)
 {
+#if DEBUG
+	cout<<u;
+#endif
+
 	if(flag_fail)
 		return;
 
@@ -60,10 +64,10 @@ void dfs(const int &u)
 	{
 		if(!color[i->to])
 		{
-			color[i->to] = 1 ^ color[i];
+			color[i->to] = 1 ^ color[u];
 			dfs(i->to);
 		}
-		else if(color[i] == color[i->to])
+		else if(color[u] == color[i->to])
 		{
 			flag_fail = true;
 #if DEBUG
@@ -92,16 +96,48 @@ int main()
 		if(!color[i])
 		{
 			color_cnt[2] = color_cnt[3] = 0;
+			color[i] = 2;
+
 			dfs(i);
+
 			if(flag_fail)
 				break;
+
+			if(color_cnt[2])
+			{
+				if(color_cnt[3])
+					ans += min(color_cnt[2],color_cnt[3]);
+				else
+					ans += color_cnt[2];
+			}
+			else if(color_cnt[3])
+				ans += color_cnt[3];
+
 		}
 	}
+
+#if DEBUG
+
+	cout<<"\n";
+
+	for(int i = 1;i <= V;++i)
+		cout<<color[i];
+
+	cout<<"\n"<<color_cnt[2]<<"\t"<<color_cnt[3]<<"\n";
+
+	if(flag_fail)
+		cout<<fault_u<<"\t"<<fault_v;
+	else
+		cout<<ans;
+
+#else
 
 	if(flag_fail)
 		cout<<"Impossible";
 	else
 		cout<<ans;
+
+#endif
 
 	return 0;
 }
