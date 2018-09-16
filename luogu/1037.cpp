9c3708c5ffd209955a27094eeab0c19fd13e326c
hajbw@his-hasee <hajbw10@126.com>
2018-09-16 20:43:38 +0800

    update luogu P1037

--- luogu/1037.cpp
+++ luogu/1037.cpp
@@ -1,22 +1,35 @@
-#include <iostream>
-#include <cstring>
-#include <set>
-
 /*
 	P1037 产生数
 */
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <set>
 
 using std::cin;
 using std::cout;
 using std::set;
 using std::string;
+using std::vector
 
 set<int> bucket[10];
+int cnt[10];
+
+int dfs(const int &u)
+{
+	if(cnt[u])
+		return cnt[u];
+	cnt[u] = 1;
+	for(set<int>::iterator it = bucket[u].begin();it != bucket[u].end;++it)
+		cnt[u] += dfs(*it);
+}
 
 int main()
 {
 	string n;
-	int k,bit,ans = 1,x,y;
+	int k,ans = 1,x,y;
 
 	cin>>n>>k;
 
@@ -27,8 +40,12 @@ int main()
 			bucket[x].insert(y);
 	}
 
-	for(string::iterator it = n.begin();i != n.end();++it)
-		ans *= bucket[*it - '0'].size() + 1;
+	for(int i = 0;i < 10;++i)
+		if(!cnt[i])
+			cnt[i] = dfs(i);
+
+	for(string::iterator it = n.begin();it != n.end();++it)
+		ans *= cnt[*it - '0'];
 
 	cout<<ans;
 
