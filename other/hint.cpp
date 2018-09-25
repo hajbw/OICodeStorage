2fef748cac7325359f618c8a5673cfd442b63a82
hajbw@his-hasee <hajbw10@126.com>
2018-09-25 18:51:28 +0800

    update hajbw int

--- other/hint.cpp
+++ other/hint.cpp
@@ -19,9 +19,11 @@
 
 #if (INT_MAX == LLONG_MAX)
 #define BIT_PER_INT 8//for 64-bit system
+#define BIT_BASE 100000000
 #define _64BIT
 #else
 #define BIT_PER_INT 4//for 32-bit system
+#define BIT_BASE 10000
 #endif
 
 #ifndef MAX_SIZE
@@ -80,8 +82,8 @@ public:
 		}
 		while(a)
 		{
-			data[index++] = a | ((1 << BIT_PER_INT + 1) - 1);
-			a >>= BIT_PER_INT;
+			data[index++] = a % BIT_BASE;
+			a /= BIT_BASE;
 		}
 
 		return *this;
@@ -94,6 +96,16 @@ public:
 	hint operator*(hint &a)
 	{
 		hint res = 0;
+		int car;
+		for(int i = 0;i < index;++i)
+		{
+			for(int j = 0;j < a.index;++j)
+			{
+				res.data[i + j] += data[i] * a.data[j];
+				car = res.data[i + j] / BIT_BASE;
+				res.data[i + j] %= BIT_BASE;
+			}
+		}
 		return res;
 	}
 
@@ -108,8 +120,8 @@ public:
 		for(int i = 0;i < len;++i)
 		{
 			data[i] += a.data[i] + carry;
-			carry = data[i] >> BIT_PER_INT;
-			data[i] |= (1 << BIT_PER_INT + 1) - 1;
+			carry = data[i] / BIT_BASE;
+			data[i] %= BIT_BASE;
 		}
 		return *this;
 	}
@@ -129,17 +141,55 @@ public:
 	{
 		struct node
 		{
-			char ch;
+			char chr;
 			node *next;
+
+			node():chr(),next(){}
+			node(char &chr,node *&next):chr(chr),next(next){}
 		}
-		*head;
+		*head,*tempnode;
 		char ch = '\0';
-		int len;
+		int len,data_len,data_xtra,temp;
 
 		a = 0;//clean a
 
-		while((ch < '0' || ch > '9') && in.good()){a.sign ^= (ch == '-');in.get(ch);}
-		while(ch >= '0' && ch <= '9' && in.good()){}
+		while((ch < '0' || ch > '9') && in.good())
+		{
+			a.sign ^= (ch == '-');
+			in.get(ch);
+		}
+		while(ch >= '0' && ch <= '9' && in.good())
+		{
+			head = new node(ch,head);
+			++len;
+			in.get(ch);
+		}
+
+		data_len = len / BIT_PER_INT - 1;
+		data_xtra = len % BIT_PER_INT;
+
+		temp = 0;
+		for(int i = data_xtra;i > 0;++i)
+		{
+			temp = (temp<<1) + (temp<<3) + head->chr - '0';
+			tempnode = head;
+			head = head->next;
+			delete tempnode;
+		}
+		a.data[data_len + 1] = temp;
+
+		for(int i = data_len;i >= 0;--i)
+		{
+			temp = 0;
+			for(int j = 5;j;--j)
+			{
+				temp = (temp<<1) + (temp<<3) + head->chr - '0';
+				tempnode = head;
+				head = head->next;
+				delete tempnode;
+			}
+			a.data[i] = temp;
+		}
 
 		return in;
 	}
