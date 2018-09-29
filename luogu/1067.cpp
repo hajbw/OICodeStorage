f8beb9ef9ff0f7105e58bb0a59cf02a0f5d4eef4
hajbw@his-hasee <hajbw10@126.com>
2018-09-29 21:43:26 +0800

    add luogu P1067(AC)

---
+++ luogu/1067.cpp
@@ -0,0 +1,46 @@
+/*
+	P1067 多项式输出
+	AC
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a,n;
+	
+	cin>>n;
+	
+	for(int i = n;i > -1;--i)
+	{
+		cin>>a;
+		
+		if(!a)
+			continue;
+		
+		if(a == -1 && i != 0)
+		{
+			cout.put('-');
+			goto outx;
+		}
+		
+		if(a > 0 && i < n)
+			cout.put('+');
+			
+		if(a != 1 || i == 0)
+			cout<<a;
+		
+outx:
+			
+		if(i == 1)
+			cout.put('x');
+		else if(i)
+			cout<<"x^"<<i;
+			
+		//cout<<'\t'<<i<<endl;
+	}
+
+	return 0;
+	
+}
