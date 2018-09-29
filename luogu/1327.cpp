71efb50905b52c7f7460eb0377d813f40ee59d6d
hajbw@his-hasee <hajbw10@126.com>
2018-09-30 07:44:43 +0800

    add luogu P1327

---
+++ luogu/1327.cpp
@@ -0,0 +1,20 @@
+/*
+	P1327 数列排序
+*/
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+const int MAXN = 100010;
+
+int main()
+{
+	int N,a;
+
+	cin>>N;
+	for(int i = 0;i < N;++i)
+	{
+		cin>>a;
+	}
+}
