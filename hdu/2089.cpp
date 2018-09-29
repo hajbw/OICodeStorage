7d6f215ecc96e111d290d7e11a006126acd2885b
hajbw@his-hasee <hajbw10@126.com>
2018-09-29 19:28:12 +0800

    update hdu P2089

--- hdu/2089.cpp
+++ hdu/2089.cpp
@@ -1,11 +1,13 @@
+/*
+	不要62
+*/
 #include <iostream>
+#include <cmath>
 
 #define min(a,b) ((a) < (b) ? (a) : (b))
 #define max(a,b) ((a) > (b) ? (a) : (b))
 
-/**
-	不要62
-*/
+#define DEBUG 1
 
 using std::cin;
 using std::cout;
@@ -13,8 +15,8 @@ using std::cout;
 const int MAXM = 1000010,MAXN = 1000010,MAXBIT = 9;
 
 int
-	M,N,bitcount,lastbitcount = 1,
-	dp[MAXBIT][10];//sum[bits - 1][first-num],dp[i][8] for a sum up
+	M,N,dp[MAXBIT][10],//sum[bits - 1][first_num],dp[i][8] for a sum up
+	sum[MAXBIT];//sum[bits - 1]
 
 template<class T> void read(T &x,std::istream &in = std::cin)
 {
@@ -37,7 +39,11 @@ inline int countbit(int x)
 
 void process()
 {
-	for(int i = lastbitcount;i < bitcount;++i)
+	//boundary conditions
+	dp[0][0] = dp[0][1] = dp[0][2] = dp[0][3] = dp[0][4] = dp[0][5] = dp[0][6] = dp[0][7] = dp[0][9] = 1;
+	sum[0] = dp[0][8] = 8;
+
+	for(int i = 1;i < MAXBIT;++i)
 	{
 		for(int j = 0;j < 10;++j)
 		{
@@ -48,44 +54,53 @@ void process()
 				dp[i][j] -= dp[i - 1][2];
 			dp[i][8] += dp[i][j];
 		}
+
+		sum[i] = sum[i - 1] + dp[i][8];
 	}
 }
 
 int getval(int n)
 {
-	int cbit = countbit(n);
-	for(int i = cbit;i;--i)
+	int cbit = countbit(n),ans = 0,nb[MAXBIT] = {};
+
+	//split n into nb
+	for(int i = 0;i < cbit;++i)
 	{
-		
+		nb[i] = n % 10;
+		n /= 10;
 	}
+
+	for(int i = cbit - 1;i;--i)//for per bit
+	{
+		for(int j = nb[i];j;--j)
+		{
+			if(j == 8)
+				continue;
+		}
+	}
+
+	return ans;
 }
 
 int main()
 {
-	std::ios::sync_with_stdio(false);
+	//std::ios::sync_with_stdio(false);
 
-	int a,b;//temp variants
-
-	//boundary conditions
-	dp[0][0] = dp[0][1] = dp[0][2] = dp[0][3] = dp[0][4] = dp[0][5] = dp[0][6] = dp[0][7] = dp[0][9] = 1;
-	dp[0][10] = 8;
+	process();
 
 	while( ~(1 ^ 1) )
 	{
 		read(N);read(M);
+
 		if(!N && !M)
 			break;
 
-		bitcount = countbit(M);
-
-		//pre-processes
-		if(bitcount > lastbitcount)
-		{
-			process();
-			lastbitcount = bitcount;
-		}
-
-		cout<<getval(M) - getval(N);
-
+#if DEBUG
+		cout<<getval(M)<<'\t'<<getval(N)<<'\n';
+#else
+		cout<<getval(M) - getval(N)<<'\n';
+#endif
 	}
+
+	return 0;
 }
