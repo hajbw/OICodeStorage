a3534936dc79fe8185a0c5c184a5d2ea35f5d4d8
hajbw@his-hasee <hajbw10@126.com>
2018-09-30 20:50:35 +0800

    add luogu P1144

---
+++ luogu/1144.cpp
@@ -0,0 +1,20 @@
+/*
+	P1144 最短路计数
+*/
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+const int MAXV = 1000010,MAXE = 2000020,ha = 100003;
+
+struct edge
+{
+	int u,v,n;
+}
+edges[MAXE<<1];
+
+int main()
+{
+	return 0;
+}
