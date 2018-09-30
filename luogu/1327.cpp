ac65c54b42a1b32d25df0150a003035655de5174
hajbw@his-hasee <hajbw10@126.com>
2018-09-30 11:51:16 +0800

    update luogu P1327

--- luogu/1327.cpp
+++ luogu/1327.cpp
@@ -2,19 +2,37 @@
 	P1327 数列排序
 */
 #include <iostream>
+#include <algorithm>
+#include <map>
 
 using std::cin;
 using std::cout;
+using std::map;
 
 const int MAXN = 100010;
+
+int arr[MAXN],brr[MAXN];
+map<int,int> order;
+
+int readInt()
+{
+	char ch = '\0';int flag = false,res = 0;
+	cin.get(ch);
+	while((ch < '0' || ch > '9') && cin.good()){flag ^= (ch == '-');cin.get(ch);}
+	while(ch >= '0' && ch <= '9'){res = (res<<1) + (res<<3) + ch - '0';cin.get(ch);}
+	if(flag)
+		res = -res;
+	return res;
+}
 
 int main()
 {
 	int N,a;
 
-	cin>>N;
+	N = readInt();
 	for(int i = 0;i < N;++i)
-	{
-		cin>>a;
-	}
+		order[arr[i] = brr[i] = readInt()] = i;
+
+	for(map<int,int>::iterator it = order.begin();it != order.end();++it)
+		cout<<it->first<<'\t'<<it->second<<'\n';
 }
