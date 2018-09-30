8bfed7e06b687f35b47952d9db616f77eb8ed2f4
hajbw@his-hasee <hajbw10@126.com>
2018-09-30 20:50:13 +0800

    add luogu P2127

---
+++ luogu/2127.cpp
@@ -0,0 +1,20 @@
+/*
+	P2127 序列排序
+*/
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+const int MAXN = 100010;
+
+int arr[MAXN];
+
+int main()
+{
+	cin>>N;
+	for(int i = 0;i < N;++i)
+	{
+		cin>>arr[i];
+	}
+}
