88614bb81a75fe993d5f895c87ccdb0f258da2dc
hajbw@his-hasee <hajbw10@126.com>
2018-10-03 22:46:20 +0800

    update luogu P3371“

--- luogu/3371.cpp
+++ luogu/3371.cpp
@@ -2,11 +2,19 @@
 	P3371 【模板】单源最短路径（弱化版）
 */
 #include <iostream>
+#include <utility>
+#include <queue>
+//use INT_MAX directly???
+//not on luogu.org :(
+#include <climits>
+
+#define min(a,b) ((a) < (b) ? (a) : (b))
 
 using std::cin;
 using std::cout;
+using std::priority_queue;
 
-const int MAXV = 10005,MAXE = 500010;
+const int MAXV = 20005,MAXE = 600010;
 
 struct edge
 {
@@ -16,18 +24,49 @@ edges[MAXE];
 
 int
 	V,E,S,
-	iedge,head[MAXV],
-	fin[MAXV],dist[MAXV];//is-finished[v],min-dist[v]
+	head[MAXV],
+	vis[MAXV],dist[MAXV];//is-finished[v],min-dist[v]
 
-void addedge(const int &u,const int &v,const int &w)
+void addedge(const int &i,const int &u,const int &v,const int &w)
 {
-	edges[++iedge] = (edge){v,w,head[u]};
-	head[u] = iedge;
+	edges[i] = (edge){v,w,head[u]};
+	head[u] = i;
 }
+
+struct cmp
+{
+	bool operator()(const int &a,const int &b)
+	{
+		return dist[a] > dist[b];
+	}
+};
 
 void dijkstra()
 {
+	//intitalize
+	priority_queue<int,std::vector<int>,cmp> quq;
+	int u,v;
 
+	for(int i = 1;i <= E;++i)
+		dist[i] = INT_MAX;
+	dist[S] = 0;
+
+	quq.push(S);
+
+	while(!quq.empty())
+	{
+		u = quq.top();
+		quq.pop();
+		if(vis[u])
+			continue;
+		vis[u] = 1;
+		for(int i = head[u];i;i = edges[i].n)
+		{
+			v = edges[i].v;
+			dist[v] = min(dist[v],dist[u] + edges[i].w);
+			quq.push(v);
+		}
+	}
 }
 
 int main()
@@ -35,9 +74,16 @@ int main()
 	int u,v,w;
 
 	cin>>V>>E>>S;
-	for(int i = 0;i < E;++i)
+	for(int i = 1;i <= E;++i)
 	{
 		cin>>u>>v>>w;
-		addedge(u,v,w);
+		addedge(i,u,v,w);
 	}
+
+	dijkstra();
+
+	for(int i = 1;i <= V;++i)
+		cout<<dist[i]<<' ';
+
+	return 0;
 }
