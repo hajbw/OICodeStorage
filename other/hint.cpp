198732ad4e7bf2a1f0b596a257d0c2620269041e
hajbw@his-hasee <hajbw10@126.com>
2018-10-04 22:34:31 +0800

    update hajbw int(unsigned)

--- other/hint.cpp
+++ other/hint.cpp
@@ -1,13 +1,201 @@
 /*
 	hajbw big int,a high percision template.
 */
+#include <iostream>
 
 #ifndef min
 #define min(a,b) ((a) < (b) ? (a) : (b))
 #endif
-
 #ifndef max
 #define max(a,b) ((a) > (b) ? (a) : (b))
+#endif
+
+using std::cin;
+using std::cout;
+
+#define _HINT
+
+#ifndef _HUINT
+#define _HUINT
+
+#ifndef min
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#endif
+#ifndef max
+#define max(a,b) ((a) > (b) ? (a) : (b))
+#endif
+
+#include <climits>
+#include <istream>
+#include <ostream>
+#include <cstring>
+
+class huint
+{
+public:
+
+	static const int MAX_SIZE = 1000,BIT_PER_INT = 4,BIT_BASE = 10000;
+
+private:
+
+	unsigned data[MAX_SIZE],index;
+
+public:
+
+	huint()
+	{
+		std::memset(data,0,sizeof(data));
+		index = 0;
+	}
+	huint(const unsigned long long &a)
+	{
+		index = MAX_SIZE;
+		*this = a;
+	}
+
+	huint& operator=(unsigned long long a)
+	{
+		std::memset(data,0,sizeof(int) * index);
+		index = 0;
+
+		while(a)
+		{
+			data[index++] = a % BIT_BASE;
+			a /= BIT_BASE;
+		}
+
+		return *this;
+	}
+
+	huint& operator+=(const huint &a)
+	{
+		unsigned car = 0;
+		index = max(index,a.index);
+		for(int i = 0;i < index;++i)
+		{
+			data[i] += a.data[i] + car;
+			car = data[i] / BIT_BASE;
+			data[i] %= BIT_BASE;
+		}
+		if(data[index])
+			++index;
+		return *this;
+	}
+
+	huint operator+(const huint &a)
+	{
+		huint res = *this;
+		res += a;
+		return res;
+	}
+
+	huint& operator-=(const huint &a)//assume that *this > a => index >= a.index
+	{
+		unsigned borrow = 0;
+		for(int i = 0;i < index;++i)
+		{
+			if(a.data[i] + borrow > data[i])
+			{
+				borrow = 1;
+				data[i] += BIT_BASE - a.data[i];
+			}
+			else
+				data[i] -= borrow + a.data[i];
+			if(i >= a.index && !borrow)
+				break;
+		}
+		return *this;
+	}
+
+	friend int greater(const huint &a,const huint &b)
+	{
+		if(a.index - b.index)
+			return (a.index - b.index) & INT_MIN ? -1 : 1 ;
+
+		//a.index == b.index
+		for(int i = a.index - 1;i > -1;--i)
+			if(a.data[i] - b.data[i])
+				return (a.data[i] - b.data[i]) & INT_MIN ? -1 : 1;
+
+		return 0;
+	}
+
+	friend bool operator==(const huint &a,const huint &b)
+	{
+		return greater(a,b);
+	}
+
+	friend bool operator<(const huint &a,const huint &b)
+	{
+		return greater(b,a) == 1;
+	}
+
+	friend bool operator>(const huint &a,const huint &b)
+	{
+		return greater(a,b) == 1;
+	}
+
+	friend bool operator>=(const huint &a,const huint &b)
+	{
+		return greater(b,a) != 1;
+	}
+
+	friend bool operator<=(const huint &a,const huint &b)
+	{
+		return greater(a,b) != 1;
+	}
+
+	friend std::istream& operator>>(std::istream &in,huint &a)
+	{
+		struct node
+		{
+			char ch;
+			node *next;
+		}
+		*head;
+		char ch = '\0';
+		int cnt = 0,temp = 0;
+
+		while((ch < '0' || ch > '9') && in.good())
+			in.get(ch);
+		while(ch >= '0' && ch <= '9' && in.good())
+		{
+			head = new node(ch,head);
+			++cnt;
+			in.get(ch);
+		}
+
+		for(int i = cnt % BIT_PER_INT;i;--i)
+		{
+			temp = (temp>>1) + (temp>>3) + head->ch;
+			head = head->next;
+		}
+
+		--cnt;
+
+		a.data[cnt/BIT_BASE] = temp;
+
+		for(int i = cnt/BIT_BASE;i > -1;--i)
+		{
+			for(int j = 0;j < 4;++j)
+			{
+				temp = (temp>>1) + (temp>>3) + head->ch;
+				head = head->next;
+			}
+			a.data[i] = temp;
+		}
+
+		return in;
+	}
+
+	friend std::ostream& operator<<(std::ostream &out,const huint &a)
+	{
+		for(int i = a.index - 1;i > -1;--i)
+			out<<a.data[i];
+		return out;
+	}
+};
+
 #endif
 
 #ifndef _HINT
@@ -211,5 +399,18 @@ public:
 
 int main()
 {
+	int a,b;
+
+	huint ha,hb;
+	cin>>a>>b;
+
+	ha = a;
+	hb = b;
+
+	cout<<ha<<'\t'<<hb<<"\n";
+
+	cout<<(ha + hb);
+	//cout<<(ha * hb)<<'\n';
+
 	return 0;
 }
