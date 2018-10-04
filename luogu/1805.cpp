d070e444ca400846841b58335c2d654e85a19cad
hajbw@his-hasee <hajbw10@126.com>
2018-10-04 15:13:38 +0800

    add luogu P1805

---
+++ luogu/1805.cpp
@@ -0,0 +1,22 @@
+/*
+	P1805 关灯
+*/
+#include <iostream>
+#include <queue>
+
+using std::cin;
+using std::cout;
+using std::queue;
+
+const int MAXN = 1010;
+
+int lights[MAXN];
+
+int main()
+{
+	int N;
+
+	cin>>N;
+	for(int i = 0;i < N;++i)
+		cin>>lights[i];
+}
