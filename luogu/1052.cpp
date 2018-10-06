b937a94a293e2410793198784af33f154e742562
hajbw@his-hasee <hajbw10@126.com>
2018-10-06 16:57:41 +0800

    add luogu P1052

---
+++ luogu/1052.cpp
@@ -0,0 +1,23 @@
+/*
+	P1052 过河
+*/
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+const int MAXL = 1000000009,MAXM = 105;
+
+int main()
+{
+	int
+		stone[MAXM] = {},
+		dp[MAXM] = {},
+		L,S,T,M;
+
+	cin>>L>>S>>T>>M;
+	for(int i = 0;i < M;++i)
+	{
+		cin>>stone[i];
+	}
+}
