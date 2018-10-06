a20a99fe106dea0056584e17e0369c945edf58b0
hajbw@his-hasee <hajbw10@126.com>
2018-10-06 19:59:42 +0800

    update luogu P1052

--- luogu/1052.cpp
+++ luogu/1052.cpp
@@ -10,14 +10,32 @@ const int MAXL = 1000000009,MAXM = 105;
 
 int main()
 {
-	int
-		stone[MAXM] = {},
-		dp[MAXM] = {},
-		L,S,T,M;
+	int	L,S,T,M;
 
 	cin>>L>>S>>T>>M;
-	for(int i = 0;i < M;++i)
+	if(S == T)
 	{
-		cin>>stone[i];
+		int stone,ans = 0;
+		for(int i = 0;i < M;++i)
+		{
+			cin>>stone;
+			ans += !(stone % S);
+		}
+		cout<<ans;
 	}
+	else
+	{
+		int
+			stone[MAXM] = {},
+			diff[MAXM] = {},
+			dp[MAXM] = {},
+
+		for(int i = 1;i <= M;++i)
+		{
+			cin>>stone[i];
+			diff[i] = stone[i] - stone[i - 1];
+		}
+	}
+
+	return 0;
 }
