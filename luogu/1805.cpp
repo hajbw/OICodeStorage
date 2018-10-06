913f802901776710cb6808761e1b1541e54948d4
hajbw@his-hasee <hajbw10@126.com>
2018-10-06 16:59:38 +0800

    git update luogu P1805

--- luogu/1805.cpp
+++ luogu/1805.cpp
@@ -9,68 +9,99 @@
 using std::cin;
 using std::cout;
 
-const int MAXN = 1010;
+const int MAXN = 1010,MAXBIT = 1000;
 
-class hint
+int n;
+struct hint
 {
-public:
-	static const int MAXL = 100,BIT_PER_INT = 8,BASE = 100000000;
+	char val[MAXBIT];
+	int index;
 
-private:
-
-	unsigned int data[MAXL],index;
-
-public:
-
-	hint(){index = 0;}
-	hint(const int &n){index = 1,data[0] = n;}
-
-	hint& operator+=(const hint &a)
+	void mul2()
 	{
-		int car = 0;
-		index = max(index,a.index);
-		for(int i = 0;i <= index;++i)
+		for(int i = 0,car = 0;i <= index;++i)
 		{
-			data[i] += a.data[i] + car;
-			car = data[i] / BASE;
-			data[i] %= BASE;
+			val[i] <<= 1;
+			val[i] += car;
+			car = val[i] / 10;
+			val[i] %= 10;
 		}
-		if(data[index])
+		if(val[index])
 			++index;
-		return *this; 
 	}
 
-	hint operator*(const hint &a)
+	hint min1()
 	{
-		hint res;
-		for(int i = 0;i < index + a.index;++i)
+		hint res = *this;
+		for(int i = 0,car = 0;i < res.index;++i)
 		{
-			
+			--res.val[i];
+			res.[i] -= car;
+			if(res.val[i] < 0)
+			{
+				res.val[i] += 10;
+				car = 1;
+			}
+			if(!car)
+				break;
 		}
 	}
 
-	hint& pow2_1(int n)//hint = 2 ^ n - 1
+	hint operator-(const hint &a)
 	{
+		int res = *this;
+		unsigned borrow = 0;
+		for(int i = 0;i < index;++i)
+		{
+			if(a.data[i] + borrow > data[i])
+			{
+				borrow = 1;
+				data[i] += BIT_BASE - a.data[i];
+			}
+			else
+				data[i] -= borrow + a.data[i];
+			if(i >= a.index && !borrow)
+				break;
+		}
+		return res;
+	}
 
+	friend std::ostream& operator<<(std::ostream &out,const hint &a)
+	{
+		for(int i = a.index - 1;i > -1;--i)
+			out.put(a.val[i] + '0');
+		return out;
 	}
 }
+f,
+pows;//2^n
 
-int lights[MAXN];
-hint f[MAXN];
+inline void minus()
+{
+	pows.min1();
+	f = pows - f;
+}
 
 int main()
 {
 	int N,light;
+	pows.val[0] = 1;//2^0
+	pows.index = f.index = 1;
 
 	cin>>N;
 	for(int i = 1;i <= N;++i)
 	{
-		cin>>lights[i];
-		if(lights[i])
+		cin>>light;
+		//if light
+		//	f[n]=2^n-1-f[n-1]
+		//else
+		//	f[n] = f[n - 1]
+		if(light)
 		{
-		}
-		else
-		{
+			while((++n) <= a)
+				pows.mul2();
+			--n;
+			minus();//-1-f[n-1]
 		}
 	}
 
