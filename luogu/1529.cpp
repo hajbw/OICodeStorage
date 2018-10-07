c4f7151ad4d18230864a9afef192bc2dd6ae6cac
hajbw@his-hasee <hajbw10@126.com>
2018-10-07 19:16:40 +0800

    add luogu P1529

--- luogu/1529.cpp
+++ luogu/1529.cpp
@@ -11,47 +11,46 @@ using std::cin;
 using std::cout;
 using std::pair;
 using std::make_pair;
-using std::priority_queue;
 
-const int MAXV = 55,MAXE = 10000,INF = 2147483647;
+const int MAXV = 130,MAXE = 10000,INF = 2147483647;
 
 struct edge
 {
 	int v,w,n;
 }
-e[MAXE];
+e[MAXE*2];
 
-int V = 52,E,iedge = 1,head[MAXV]
-
-inline int convert(char ch)
-{
-	if(ch >= 'a' && ch <= 'z')//['a','z'] => [1,26]
-		return ch - 'a' + 1;
-	if(ch >= 'A' && ch <= 'Z')//['A','Z'] => [27,52]
-		return ch - 'A' + 27;
-	return 0;
-}
+int V = 52,E,iedge = 1,head[MAXV],dist[MAXV];
 
 pair<int,int> bfs()
 {
-	priority_queue<pair<int,int>> quq;//dist,v
+	std::queue<pair<int,int>> quq;//dist,v
 	pair<int,int> ans(INF,0);
 
-	quq.push(make_pair(0,convert('Z')));
+	quq.push(make_pair(0,(int)'Z'));
 
 	while(!quq.empty())
 	{
-		pair<int,int> u = quq.top();
+		pair<int,int> u = quq.front();
 		quq.pop();
 
-		if(u.second > 26 && u.second < 52)
-			ans = min(ans,u);
-		else
-			for(int i = head[u.second];i;i = e[i].n)
-				quq.push(make_pair(u.first + e[i].w,e[i].v));
+		dist[u.second] = u.first;
 
-		return ans;
+		if(u.second >= 'A' && u.second < 'Z')
+			ans = min(ans,u);
+//		else
+//		{
+			for(int i = head[u.second];i;i = e[i].n)
+			{
+				if(dist[e[i].v])
+					dist[e[i].v] = min(dist[e[i].v],u.first + e[i].w);
+				else
+					quq.push(make_pair(u.first + e[i].w,e[i].v));
+			}
+//		}
 	}
+
+	return ans;
 }
 
 inline void addedge(int u,int v,int w)
@@ -71,13 +70,13 @@ int main()
 	for(int i = 0;i < E;++i)
 	{
 		cin>>u>>v>>w;
-		addedge(convert(u),convert(v),w);
-		addedge(convert(v),convert(u),w);
+		addedge(u,v,w);
+		addedge(v,u,w);
 	}
 
 	ans = bfs();
 
-	cout<<char(ans.first - 27 + 'A')<<' '<<ans.second;
+	cout<<(char)ans.second<<' '<<ans.first;
 
 	return 0;
 }
