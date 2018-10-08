fb6679b193b8f7aab8837a358018e8837ca75c92
hajbw@his-hasee <hajbw10@126.com>
2018-10-08 09:05:55 +0800

    add luogu P2921

--- luogu/2921.cpp
+++ luogu/2921.cpp
@@ -11,43 +11,53 @@ using std::cout;
 
 const int MAXN = 1000010;//MAXV = MAXE
 
-int next[MAXN],dfn[MAXN],low[MAXN],jzm,ans[MAXN];
+int next[MAXN],dfn[MAXN],low[MAXN],vis[MAXN],jzm,ans[MAXN];
 std::stack<int> st,scc;
 
 void tarjan(int u)
 {
 	dfn[u] = low[u] = ++jzm;
+	++vis[u];
 	st.push(u);
 
-	if(!dfn[next[u]])
+	if(!vis[next[u]])
 	{
 		tarjan(next[u]);
 		low[u] = min(low[u],low[next[u]]);
 	}
-	else
+	else if(vis[next[u]] == 1)
 		low[u] = min(low[u],dfn[next[u]]);
 
-	if(low[u] == dfn[u])
+	if(low[u] != dfn[u])
+		return;
+
+	int v,cnt = 0;
+	do
 	{
-		int v,cnt = 0;
-		do
-		{
-			v = st.top();
-			scc.push(v);
-			st.pop();
-			++cnt;
-		}
-		while(u != v);
+		v = st.top();
+		st.pop();
+		scc.push(v);
+		++vis[v];
+		++cnt;
+	}
+	while(u != v);
+
+	if(ans[u] == 1)
+		return;
+
+	if(cnt == 1)
+		ans[u] = ans[next[u]] + 1;
+	else
+	{
 		do
 		{
 			v = scc.top();
 			scc.pop();
-			ans[v] = cnt;
+			ans[u] = cnt;
 		}
 		while(!scc.empty());
 	}
-	else
-		ans[u] = ans[next[u]] + 1;
+
 }
 
 int main()
@@ -56,7 +66,11 @@ int main()
 
 	cin>>N;
 	for(int i = 1;i <= N;++i)
+	{
 		cin>>next[i];
+		if(next[i] == i)
+			ans[i] = 1;
+	}
 
 	for(int i = 1;i <= N;++i)
 		if(!dfn[i])
