e7f2e8e4ba7e7223a4aab6deb8a413196fdf7500
hajbw@his-hasee <hajbw10@126.com>
2018-10-17 08:54:03 +0800

    add luogu P1168

---
+++ luogu/1168.cpp
@@ -0,0 +1,31 @@
+/*
+	P1168 中位数
+*/
+#include <iostream>
+#include <queue>
+
+using std::cin;
+using std::cout;
+
+const int MAXN = 100010;
+
+int N;
+
+int main()
+{
+	int a;
+
+	cin>>N;
+	for(int i = 1;i <= N;++i)
+	{
+		cin>>a;
+		//quq.push(a);
+
+		if(i % 2)
+		{
+			
+		}
+	}
+
+	return 0;
+}
