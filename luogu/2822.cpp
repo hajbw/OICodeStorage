902a53299de89d0209727f2d2acd42bf02359c17
hajbw@his-hasee <hajbw10@126.com>
2018-10-21 09:22:52 +0800

    update luogu P2822

--- luogu/2822.cpp
+++ luogu/2822.cpp
@@ -1,5 +1,6 @@
 /**
 	P2822 组合数问题
+	AC
 */
 
 #include <iostream>
@@ -13,40 +14,51 @@ using std::cout;
 
 const int MAXN = 2018;
 
-unsigned long long C[MAXN][MAXN];
+unsigned long long
+	C[MAXN][MAXN],
+	cntC[MAXN][MAXN];//
 
 int main()
 {
-	int t,n,m,o,k,ans;
-
-	for(int i = 0;i < MAXN;++i)
-		C[i][0] = C[0][i] = 1;
+	int t,n,m,k,ans;
 
 	cin>>t>>k;
 
+	for(int i = 0;i < MAXN;++i)
+		C[i][0] = 1;
+
 	for(int i = 1;i < MAXN;++i)
+	{
 		for(int j = 1;j <= i;++j)
-			cout<<i<<'\t'<<j<<'\t'<<(C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % k)<<'\n';
+		{
+			C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % k;
+			cntC[i][j] = cntC[i][j - 1] + !C[i][j];
+		}
+	}
+
+/*	for(int i = 0;i < 11;++i)
+	{
+		for(int j = 0;j <= i;++j)
+			cout<<C[i][j]<<'\t';
+		cout<<'\n';
+	}
+
+	cout<<"\n---------------------------------------\n";
+
+	for(int i = 0;i <= 10;++i)
+	{
+		for(int j = 0;j <= i;++j)
+			cout<<cntC[i][j]<<'\t';
+		cout<<'\n';
+	}*/
 
 	while(t--)
 	{
 		ans = 0;
 		cin>>n>>m;
-		for(int i = 0;i < n;++i)
-		{
-			o = min(i,m);
-			for(int j = 0;j < o;++j)
-			{
-				if(!C[i][j])
-				{
-					++ans;
-#if DEBUG
-					cout<<i<<'\t'<<j<<'\n';
-#endif
-				}
-			}
-		}
-		cout<<ans;
+		for(int i = 1;i <= n;++i)
+			ans += cntC[i][min(i,m)];
+		cout<<ans<<'\n';
 	}
 	
 	return 0;
