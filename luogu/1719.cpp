7897b02257f54151d96664ee855babdbd2492f45
hajbw@his-hasee <hajbw10@126.com>
2018-10-21 22:03:46 +0800

    update luogu P1719(AC)

--- luogu/1719.cpp
+++ luogu/1719.cpp
@@ -1,5 +1,6 @@
 /*
 	P1719 最大加权矩形
+	AC
 */
 #include <iostream>
 
@@ -8,7 +9,12 @@ using std::cout;
 
 const int MAXN = 128;
 
-int N,matrix[MAXN][MAXN],sum[MAXN][MAXN];
+int N,sum[MAXN][MAXN],curr,ans = -19260817;
+
+template<class T>inline T max(const T &a,const T &b)
+{
+	return a > b ? a : b;
+}
 
 int main()
 {
@@ -17,8 +23,25 @@ int main()
 	{
 		for(int j = 1;j <= N;++j)
 		{
-			cin>>matrix[i][j];
-			sum[i][j] = sum[i][j - 1] + matrix[i][j];
+			cin>>curr;
+			sum[i][j] = sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1] + curr;
 		}
 	}
+
+	/*for(int i = 1;i <= N;++i)
+	{
+		for(int j = 1;j <= N;++j)
+			cout<<sum[i][j]<<'\t';
+		cout<<'\n';
+	}*/
+
+	for(int i1 = 1;i1 <= N;++i1)
+		for(int j1 = 1;j1 <= N;++j1)
+			for(int i2 = 0;i2 < i1;++i2)
+				for(int j2 = 0;j2 < j1;++j2)
+					ans = max(ans,sum[i1][j1] - sum[i1][j2] - sum[i2][j1] + sum[i2][j2]);
+
+	cout<<ans;
+
+	return 0;
 }
