128314be66962c73bbd4454bdb62ec576339341e
hajbw@his-hasee <hajbw10@126.com>
2018-10-21 18:18:07 +0800

    add luogu P1083

---
+++ luogu/1083.cpp
@@ -0,0 +1,64 @@
+/*
+	P1083 借教室
+*/
+#include <iostream>
+#include <cstring>
+
+using std::cin;
+using std::cout;
+
+const int MAXM = 100010,MAXN = 100010;
+
+long long r[MAXN],d[MAXM],diff[MAXN],sum;
+int M,N,s[MAXM],t[MAXM],flag;
+
+inline bool satisfy(int m)
+{
+	sum = 0;
+	std::memset(diff,0,sizeof(long long) * (N + 2));
+
+	for(int i = 1;i <= m;++i)
+	{
+		diff[s[i]]		+= d[i];
+		diff[t[i] + 1]	-= d[i];
+	}
+
+	for(int i = 1;i <= N;++i)
+	{
+		sum += diff[i];
+		if(sum > r[i])
+			return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	cin>>N>>M;
+
+	for(int i = 1;i <= N;++i)
+		cin>>r[i];
+
+	for(int i = 1;i <= M;++i)
+		cin>>d[i]>>s[i]>>t[i];
+
+	int lb = 1,rb = M + 1,mid;
+
+	while(rb - lb > 1)
+	{
+		mid = lb + rb >> 1;
+
+		if(satisfy(mid))
+			lb = mid;
+		else
+			rb = mid;
+	}
+
+	if(lb == M)
+		cout<<0;
+	else
+		cout<<"-1\n"<<rb;
+
+	return 0;
+}
