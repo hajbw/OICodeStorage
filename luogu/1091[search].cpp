c1de3239b89983711db84ecf05b0152ef8fb5220
hajbw@his-hasee <hajbw10@126.com>
2018-10-22 11:21:42 +0800

    update luogu P1091

--- luogu/1091[search].cpp
+++ luogu/1091[search].cpp
@@ -3,30 +3,42 @@
 	brute-force search
 */
 #include <iostream>
+#include <deque>
 
 using std::cin;
 using std::cout;
+using std::deque;
 
 const int MAXN = 105;
 
-int arr[MAXN],N,vis[MAXN];
+int arr[MAXN],N,ans = 19260817;
+deque<int> quq;
 
 void dfs(int n,int cnt)
 {
 	if(n > N)
 	{
-		for(int i = 1;i <= N;++i)
+		deque<int>::iterator it;
+		for(it = quq.begin();it != quq.end();++it)
+			cout<<*it<<'\t';
+		cout<<'\n';
+		for(it = quq.begin();it != quq.end() - 1 && *it > *(it + 1);++it) ;
+		for(;it != quq.end() - 1 && *it <= *(it + 1);++it) ;
+		cout<<'\n';
+		if(it == quq.end() - 1)
 		{
-			if(vis[i])
-				continue;
-
+			cout<<cnt<<'\n';
+			if(cnt < ans)
+				ans = cnt;
 		}
+
+		return;
 	}
 
-	dfs(n + 1)
-	vis[n] = 1;
-	dfs(n + 1);
-	vis[n] = 0;
+	dfs(n + 1,cnt + 1);
+	quq.push_back(arr[n]);
+	dfs(n + 1,cnt);
+	quq.pop_back();
 }
 
 int main()
@@ -34,6 +46,10 @@ int main()
 	cin>>N;
 	for(int i = 1;i <= N;++i)
 		cin>>arr[i];
+
+	dfs(1,0);
+
+	cout<<ans;
 
 	return 0;
 }
