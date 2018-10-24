0fe02a4ea5e5b008e8053d5f7df5ec6fe20c13b8
hajbw@his-hasee <hajbw10@126.com>
2018-10-24 21:28:36 +0800

    update lugou P1020 (AC)

--- luogu/1020.cpp
+++ luogu/1020.cpp
@@ -1,16 +1,20 @@
 /*
 	P1020 导弹拦截
+	AC
 */
 #include <iostream>
 #include <cstring>
+#include <utility>
 #include <algorithm>
 
 using std::cin;
 using std::cout;
 
-const int MAXN = 200020;
+const int MAXN = 200020,INF = 0x7f7f7f7f;
 
 int arr[MAXN],acs[MAXN],des[MAXN],cnt;
+std::greater<int> rev_cmp;
+bool wtf_cmp(int a,int b){return b == 0;}
 
 template<class T> bool read(T &x)
 {
@@ -25,6 +29,21 @@ template<class T> bool read(T &x)
 
 int main()
 {
-	while(cin>>arr[cnt++]) ;
+	while(read(arr[cnt++])) ;
 
+	--cnt;
+
+	std::memset(acs,0x7f,sizeof(int) * cnt);
+
+	for(int i = 0;i < cnt;++i)
+		*std::upper_bound(des,des + i + 1,arr[i],rev_cmp) = arr[i];
+
+	cout<<std::lower_bound(des,des + cnt + 1,0,rev_cmp) - des<<'\n';
+
+	for(int i = 0;i < cnt;++i)
+		*std::lower_bound(acs,acs + i + 1,arr[i]) = arr[i];
+
+	cout<<std::lower_bound(acs,acs + cnt + 1,INF) - acs<<'\n';
+
+	return 0;
 }
