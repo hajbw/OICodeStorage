de1f8b67d81582fcd053d311866ee6404af100fc
hajbw@his-hasee <hajbw10@126.com>
2018-10-24 19:00:49 +0800

    update luogu P1941

--- luogu/1941.cpp
+++ luogu/1941.cpp
@@ -4,47 +4,126 @@
 #include <iostream>
 #include <cstring>
 
+#define DEBUG 1
+
 using std::cin;
 using std::cout;
 
-const int MAXN = 10005,MAXM = 1000;
+const int MAXN = 10005,MAXM = 1000,INF = 2139062143;
 
-struct hpair
+int
+	M,N,K,
+	X[MAXN],Y[MAXN],up[MAXN],down[MAXN],tube[MAXN],
+	dp[MAXN][MAXM];
+
+template<class T>inline T min(T a,T b)
 {
-	int x,y;
-
-	friend std::istream& operator>>(std::istream &in,hpair &a)
-	{
-		return in>>a.x>>a,y;
-	}
+	return a < b ? a : b;
 }
-delta[MAXN],//X[x],Y[x]
-map[MAXN];//lower_bound[x],upper_bound[x]
 
-int M,N,K,dp[MAXN][MAXM];
+template<class T>inline T max(T a,T b)
+{
+	return a > b ? a : b;
+}
 
 int main()
 {
+	register int flag,a,i,j;
+
 	cin>>N>>M>>K;
-	for(int i = 1;i <= N;++i)
-		cin>>delta[i];
-	for(int i = 1,p;i <= K;++i)
-		cin>>p>>map[p];
-
-	for(int i = 1;i <= M;++i)
-		if(!map[i].y)
-			map[i].y = m;
-
-	for(int i = 1;i <= N;++i)
+	for(i = 1;i <= N;++i)
+		cin>>X[i]>>Y[i];
+	for(i = 1;i <= K;++i)
 	{
-		for(int j = map[i].x;j <= map[i].y;++j)
+		cin>>a>>down[a]>>up[a];
+		tube[a] = 1;
+	}
+
+	for(i = 1;i <= M;++i)
+		if(!tube[i])
+			up[i] = M;
+
+	cout<<'\n';
+	for(int i = 0;i <= N;++i)
+		cout<<down[i]<<'\t';
+	cout<<'\n';
+	for(int i = 0;i <= N;++i)
+		cout<<up[i]<<'\t';
+	cout<<'\n';
+	for(int i = 0;i <= N;++i)
+		cout<<tube[i]<<'\t';
+	cout<<'\n';
+
+	std::memset(dp,127,sizeof(dp));
+	std::memset(dp,0,sizeof(dp[0]));
+
+	for(i = 0;i <= N;++i)
+	{
+		cout<<" i:"<<i<<' ';
+		//going up
+		for(j = max(down[i] + 1,down[i - 1] + X[i - 1]);j < up[i];++j)
 		{
-			
+			cout<<" 1j:"<<j<<' ';
+			dp[i][j] = min(dp[i - 1][j - X[i - 1]] + 1,dp[i][j - X[i - 1]] + 1);
 		}
 
-		for(int j = map[i].x;j <= map[i].y;++j)
+		//falling down
+		a = min(up[i],up[i - 1] - Y[i - 1]);
+		//cout<<" a:"<<a<<' ';
+		printf(" %d,%d,%d-%d ", a,up[i],up[i - 1],Y[i - 1]);
+		for(j = down[i] + 1;j < a;++j)
 		{
-			
+			cout<<" 2j:"<<j<<' ';
+			dp[i][j] = min(dp[i][j],dp[i - 1][j + Y[i - 1]]);
+		}
+
+		//specially for upper bound
+		//when no pipe,able to touch top
+		if(!tube[i])
+		{
+			for(j = 1;j <= X[i];++j)
+				dp[i][M] = max(dp[i][M],dp[i][M - j] + 1);
+		}
+
+		//if unable
+		for(j = down[i] + 1;j < up[i] && dp[i][j] >= INF;++j) ;
+
+		if(j == up[i])
+		{
+			a = i;
+			flag = true;
+			break;
 		}
 	}
+
+#if DEBUG
+
+	cout<<'\n';
+
+	for(j = M;j >= 0;--j)
+	{
+		for(i = 0;i <= N;++i)
+		{
+			if(dp[i][j] >= INF)
+				cout<<"INF\t";
+			else
+				cout<<dp[i][j]<<'\t';
+		}
+		cout<<'\n';
+	}
+
+
+#endif
+
+	if(flag)
+		cout<<"0\n"<<a;
+	else
+	{
+		a = INF;
+		for(int i = down[N] + 1;i < up[N];++i)
+			a = min(a,dp[N][i]);
+		cout<<"1\n"<<a;
+	}
+
+	return 0;
 }
