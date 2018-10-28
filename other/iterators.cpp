90e39311ed5d1ba39726b940023a51fca7c6f6f8
hajbw@IT3@school <hajbw10@126.com>
2018-10-28 22:05:53 +0800

    update iterator

--- other/iterators.cpp
+++ other/iterators.cpp
@@ -10,15 +10,17 @@
 template<class input_t,class return_t>class hfunc_object
 {
 public:
+
 	return_t operator()(input_t i);
+
+	bool available(input_t i)const; 
 };
 
 template<class return_t>class hfunc_linear : hfunc_object<int,return_t>
 {
+	input_t min_val,max_val;
 
 public:
-
-	return_t operator()(int i);
 
 	class iterator : public std::iterator<std::random_access_iterator_tag,hfunc_linear<return_t>>
 	{
@@ -33,10 +35,14 @@ public:
 		//increment/decrement implements
 		iterator& operator++(){++x;return *this;}
 		iterator& operator--(){--x;return *this;}
-		iterator operator++(int){iterator temp(*this);++x;return temp;}
-		iterator operator--(int){iterator temp(*this);--x;return temp;}
+		iterator operator++(int){iterator tmp(*this);++x;return tmp;}
+		iterator operator--(int){iterator tmp(*this);--x;return tmp;}
 		iterator& operator+=(int i){x += i;return *this;}
 		iterator& operator-=(int i){x -= i;return *this;}
+
+		//numberic operations
+		iterator operator+(int i){iterator res(*this);return ++res;}
+
 
 		//comparisions
 		bool operator==(const iterator &a)const{return x == a.x;}
@@ -47,9 +53,20 @@ public:
 		bool operator>=(const iterator &a)const{return x >= a.x;}
 
 		//dereference operators
-		return_t operator*()const{return (*hfunc_linear::this)(x);}
+		return_t operator*()const{;return (*hfunc_linear::this)(x);}
 		return_t operator[](int a)const{return (*hfunc_linear::this)(x + a);}
 	};
+
+	return_t operator()(int i);
+
+	bool available(input_t i)const
+	{
+		return i <= max_val && i > min_val;
+	}
+
+	iterator begin(){return iterator(min_val);}
+
+	iterator end(){return iterator(max_val + 1);}
 
 };
 
