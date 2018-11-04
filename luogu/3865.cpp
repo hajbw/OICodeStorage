bb937427a94a1142129422c98a67ee055fe44de2
hajbw@his-hasee <hajbw10@126.com>
2018-11-04 15:50:03 +0800

    update luogu P3865(AC)

--- luogu/3865.cpp
+++ luogu/3865.cpp
@@ -1,5 +1,6 @@
 /*
 	P3865 【模板】ST表
+	AC
 */
 #include <iostream>
 #include <cmath>
@@ -7,39 +8,36 @@
 using std::cin;
 using std::cout;
 
-const int MAXN = 10010,MAXM = 100000;
+const int MAXN = 100010,MAXM = 1000010;
 
-int lg2[MAXN],sparse[MAXN][16],arr[MAXN],N,M;
+int lg2[MAXN],sparse[MAXN][20],arr[MAXN],N,M;
 
-template<class T> inline T min(const T &a,const T &b)
-{
-	return a < b ? a : b;
-}
-
-template<class T> inline T max(const T &a,const T &b)
-{
-	return a > b ? a : b;
-}
+template<class T> inline const T& max(const T &a,const T &b){return a > b ? a : b;}
 
 int main()
 {
+	std::ios::sync_with_stdio(false);
+
+	cin.tie(NULL);
+
 	cin>>N>>M;
 	for(int i = 1;i <= N;++i)
 		cin>>sparse[i][0];
 
 	lg2[2] = 1;
 	for(int i = 3;i <= N;++i)
-		lg2[i] = lg2[i >> 1] + 1;
+		lg2[i] = lg2[(i >> 1)] + 1;
 
-	for(int i,j = 0,t = 1;t <= N,j < 16;++j,t <<= 1)//t == 1 << j
-		for(i = 0;i + (t << 1) <= N;++i)
-			sparse[i][j + 1] = max(sparse[i][j],sparse[i + t][j]);
+	for(int i = 0;1 << i <= N;++i)
+		for(int j = 1;j + (1 << i) <= N;++j)
+			sparse[j][i + 1] = max(sparse[j][i],sparse[j + (1 << i)][i]);
 
-	for(int i = 1,j,l,r;i <= M;++i)
+	for(int i = 1,l,r,range;i <= M;++i)
 	{
 		cin>>l>>r;
-		j = lg2[r - l + 1];
-		cout<<max(sparse[l][j],sparse[r - (1 << j) + 1][j])<<'\n';
+		range = lg2[r - l + 1];
+		//cout<<sparse[l][range]<<'\t'<<sparse[r - (1 << range) + 1][range]<<'\n';
+		cout<<max(sparse[l][range],sparse[r - (1 << range) + 1][range])<<'\n';
 	}
 
 	return 0;
