e5f744e31700804585dc4b144f886c504731f45f
hajbw@his-hasee <hajbw10@126.com>
2018-11-06 16:13:12 +0800

    update luogu P1030(AC)

--- luogu/1030.cpp
+++ luogu/1030.cpp
@@ -1,29 +1,56 @@
-#include <iostream>
-
-/**
+/*
 	P1030 求先序排列
+	AC
 */
+
+#include <iostream>
+#include <cstring>
 
 using std::cin;
 using std::cout;
 
-struct node
+inline char* strend(char *s)
 {
-	char name;
-	node *left,*right;
+	while(*s)
+		++s;
+	return s;
 }
-*root;
 
-char prevorder[10],inorder[10],postorder[10];
+char* get_preorder(char *inorder_begin,char *postorder_begin,char *postorder_end)
+{
+	if(postorder_end - postorder_begin < 1)
+		return new char[1]{'\0'};
+	if(postorder_end - postorder_begin == 1)
+		return new char[2]{*postorder_begin,'\0'};
+
+	char *iin,*res = new char[postorder_end - postorder_begin]{};
+
+	iin = strchr(inorder_begin,postorder_end[-1]);
+
+	int len = iin - inorder_begin;
+
+	res[0] = *iin;
+
+	*iin = '\0';
+
+	strcpy(strend(res),get_preorder(inorder_begin,postorder_begin,postorder_begin + len));
+	
+	strcpy(strend(res),get_preorder(iin + 1,postorder_begin + len,postorder_end - 1));
+
+	return res;
+}
 
 int main()
 {
+	char inorder[10],postorder[10],*temp;
 	cin.getline(inorder,10);
 	cin.getline(postorder,10);
 
-	int index = strlen(inorder);
+	//on win
+	//cout<<get_preorder(inorder,postorder,strend(postorder)) + 1;
 
-	
+	//on luogu
+	cout<<get_preorder(inorder,postorder,strend(postorder)) + 1;
 
 	return 0;
 }
